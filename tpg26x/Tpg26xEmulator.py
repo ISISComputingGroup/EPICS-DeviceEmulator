304f66acde3286e5c8f9254d4330f8dc04d798c9
John-Holt-Tessella <John.Holt@tessella.com>
2016-07-26 09:53:24 +0100

    Refactor emulator to make operation clearer

--- tpg26x/Tpg26xEmulator.py
+++ tpg26x/Tpg26xEmulator.py
@@ -1,42 +1,81 @@
 import sys
+from telnet_engine import TelnetEngine, ENQ_SIGNAL, ACK_SIGNAL, NAK_SIGNAL
+
+"""Prefix for command sent to emulator to change the state of the emulator"""
+EMULATOR_COMMAND_PREFIX = "emulator:"
 
 sys.path.append("../test_framework")
 
 
-from telnet_engine import TelnetEngine
-
-
 class Tpg26xEmulator:
+    """
+    Emulator for the TPG26x pressure reading device
+    """
+
     def __init__(self):
+        """
+        Constructor
+        :return:
+        """
         self.pressure2 = 0.08
         self.pressure1 = 0.3
         self.error2 = 1
         self.error1 = 0
         self.params = []
         self.enquire = None
+        self.units = 1
 
     def process(self, data):
         """
-        process the incoming data
-        :param data: data
+        process the incoming data and return the answer
+        :param data: data sent
             :type data: str
-        :return:
+        :return: response
         """
-        if data == chr(5):
-            return self._make_enquirey()
-        else:
-            self.enquire = None
-            self.params = None
-            tokens = data.split(',')
-            if len(tokens) > 0:
-                self.enquire = tokens[0]
-            if len(tokens) > 1:
-                self.params = tokens[1:]
+        try:
+            if data.startswith(EMULATOR_COMMAND_PREFIX):
+                return self._do_emulator_command(data[len(EMULATOR_COMMAND_PREFIX):])
+            elif data == chr(5):
+                return self._make_enquiry()
+            else:
+                self.enquire = None
+                self.params = None
+                tokens = data.split(',')
+                if len(tokens) > 0:
+                    self.enquire = tokens[0]
+                    return ENQ_SIGNAL
+                if len(tokens) > 1:
+                    return self._set_value(tokens[1:])
+                return None
 
-            return "\x06"
+        except (ValueError, TypeError) as ex:
+            print "Exception thrown during emulation: {0}".format(ex)
 
+    def _do_emulator_command(self, command):
+        """
+        Perform the emulator state change based on the command
+        :param command: command that was sent (without the prfix)
+        :return: response
+        """
+        parameters = command.split()
+        if parameters[0] == "set:pressure1":
+            self.pressure1 = float(parameters[1])
+        elif parameters[0] == "set:pressure2":
+            self.pressure2 = float(parameters[1])
+        elif parameters[0] == "set:error1":
+            self.error1 = int(parameters[1])
+        elif parameters[0] == "set:error2":
+            self.error2 = int(parameters[1])
+        elif parameters[0] == "set:units":
+            self.units = int(parameters[1])
+        return None
 
-    def _make_enquirey(self):
+    def _make_enquiry(self):
+        """
+        Most commands sent are in two parts a command which either sets or asks for a value and then
+        an enquirey that gets the response. This function deals with the response to an enquiry
+        :return: response
+        """
         if self.enquire == "PRX":
             #x,sx.xxxxEsxx,y,sy.yyyyEsyy
             return "{error1:1d},{pressure1:+011.4E},{error2:1d},{pressure2:011.4E}".format(
@@ -46,9 +85,22 @@ class Tpg26xEmulator:
                 pressure2=self.pressure2
             )
         elif self.enquire == "UNI":
-            return "1"
+            return "{0:1d}".format(self.units)
+
+    def _set_value(self, values):
+        """
+        Set a value based on an instruction from the IOC
+        :param values: value to set
+        :return: response
+        """
+        if self.enquire == "UNI":
+            self.units = int(values[0])
+            return ACK_SIGNAL
+        else:
+            print "Error trying to set {0} which the simulator can not".format(self.enquire)
+            return NAK_SIGNAL
 
 if __name__ == "__main__":
     port_file = __file__
-    dev = tpg26xEmulator()
+    dev = Tpg26xEmulator()
     TelnetEngine().start(dev, port_file)
