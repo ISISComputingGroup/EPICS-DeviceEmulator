c715ef1656e6ea7d895f21e56c9b1d059849fbad
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-04-20 11:17:40 +0100

    Moved state into its own file

--- lewis_emulators/HRPD_Sample_changer/device.py
+++ lewis_emulators/HRPD_Sample_changer/device.py
@@ -1,21 +1,8 @@
 from lewis.devices import StateMachineDevice
 from lewis.core.statemachine import State
-from lewis.core import approaches
+from states import MovingState
 
 from collections import OrderedDict
-
-
-class MovingState(State):
-    def on_entry(self, dt):
-        self._context.arm_lowered = False
-
-    def in_state(self, dt):
-        old_position = self._context.car_pos
-        self._context.car_pos = approaches.linear(old_position, self._context.car_target,
-                                                   self._context.SPEED, dt)
-
-    def on_exit(self, dt):
-        self._context.arm_lowered = True
 
 
 class SimulatedHRPDSampleChanger(StateMachineDevice):
---
+++ lewis_emulators/HRPD_Sample_changer/states.py
@@ -0,0 +1,15 @@
+from lewis.core import approaches
+from lewis.core.statemachine import State
+
+
+class MovingState(State):
+    def on_entry(self, dt):
+        self._context.arm_lowered = False
+
+    def in_state(self, dt):
+        old_position = self._context.car_pos
+        self._context.car_pos = approaches.linear(old_position, self._context.car_target,
+                                                   self._context.SPEED, dt)
+
+    def on_exit(self, dt):
+        self._context.arm_lowered = True
