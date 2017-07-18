165ed4e7410620912af8b3ecbb85c1f3c183f53d
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-07-18 14:01:26 +0100

    Lewis can't handle not having a termination character

--- lewis_emulators/fermichopper/interfaces/stream_interface.py
+++ lewis_emulators/fermichopper/interfaces/stream_interface.py
@@ -5,15 +5,16 @@ class FermichopperStreamInterface(StreamAdapter):
 
     # Commands that we expect via serial during normal operation
     commands = {
-        Cmd("get_zero", "^i_want_zero$"),
+        Cmd("get_data", "^#0000000\$$"),
     }
 
-    in_terminator = ""
-    out_terminator = ""
+    in_terminator = "\n"
+    out_terminator = "\n"
 
     def handle_error(self, request, error):
         print "An error occurred at request " + repr(request) + ": " + repr(error)
         return str(error)
 
-    def get_zero(self):
-        return 0
+    def get_data(self):
+        # This is "known good" example data from the documentation.
+        return "#10003F4#2003F0B#3177002#4177003#55F9019#60001F7#75F8B2C#80001F9#90012FC#A00C81C#B020004#C012C19#D012C1A#E012C1B$"
