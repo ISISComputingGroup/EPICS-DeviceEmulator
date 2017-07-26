78917d31978093338eecff55706687b0dae6be70
Jack Draper <drapj002@gmail.com>
2017-07-26 11:03:09 +0100

    Added more commands for AG33220A emulator & fixed function command

--- lewis_emulators/ag33220a/device.py
+++ lewis_emulators/ag33220a/device.py
@@ -2,6 +2,12 @@ from lewis.devices import Device
 
 
 class SimulatedAG33220A(Device):
+    make = "Agilent Technologies,33220A"
+    firmwareRevNum = "MY44033103,2.02"
+    bootKernelRevisionNumber = "2.02"
+    asicRevNum = "22"
+    printedCircuitBoardRevNum = "2"
+    idn = make+"-"+firmwareRevNum+"-"+bootKernelRevisionNumber+"-"+asicRevNum+"-"+printedCircuitBoardRevNum
     voltage = 1
     frequency = 1000
     offset = 0
--- lewis_emulators/ag33220a/interfaces/stream_interface.py
+++ lewis_emulators/ag33220a/interfaces/stream_interface.py
@@ -1,10 +1,4 @@
 from lewis.adapters.stream import StreamAdapter, Cmd
-import random
-
-SP_COMM = "spv"
-UNITS_COMM = "uiu"
-READING_COMM = "r"
-
 
 class AG33220AStreamInterface(StreamAdapter):
 
@@ -21,46 +15,30 @@ class AG33220AStreamInterface(StreamAdapter):
         Cmd("set_function", "^FUNC " + "(SIN|SQU|RAMP|PULS|NOIS|DC|USER)$", argument_mappings=[str]),
         Cmd("get_output", "^OUTP\?$"),
         Cmd("set_output", "^OUTP " + "(0|1|ON|OFF)$", argument_mappings=[str]),
+        Cmd("get_idn", "^\*IDN\?$"),
 
     }
 
     in_terminator = "\n"
     out_terminator = "\n"
 
-    #out_echo = "33220A"#*{}*:{};{}"
-    #out_response = "!{}!o!"
-
-#    def create_response(self,):
-    #def create_response(self, command, params=" ", data=None):
-    #    out = self.out_echo.format(self._device.address, command, params) + self.out_terminator
-     #   if data:
-      #      out += data + self.out_terminator
-       # return out
-
     def get_voltage(self):
-#        print "Device voltage is: " + str(self._device.voltage)
         return self._device.voltage
 
     def set_voltage(self, new_voltage):
         self._device.voltage = new_voltage
-#        print "Device voltage is: " + str(self._device.voltage)
-#        return self.create_response("VOLT")
 
     def get_freq(self):
-#        print "1. Device frequency is: " + str(self._device.frequency)
         return self._device.frequency
 
     def set_freq(self, new_frequency):
         self._device.frequency = new_frequency
-#        print "2. Device frequency is: " + str(self._device.frequency)
 
     def get_offset(self):
-        #print "1. Device offset is: " + str(self._device.offset)
         return self._device.offset
 
     def set_offset(self, new_offset):
         self._device.offset = new_offset
-        #print "2. Device offset is: " + str(self._device.offset)
 
     def get_units(self):
         return self._device.units
@@ -69,13 +47,12 @@ class AG33220AStreamInterface(StreamAdapter):
         self._device.units = new_units
 
     def get_function(self):
-        return self._device.funtion
+        return self._device.function
 
     def set_function(self, new_function):
         self._device.function = new_function
 
     def get_output(self):
-        print str(self._device.output) + "...3"
         return self._device.output
 
     def set_output(self, new_output):
@@ -85,7 +62,8 @@ class AG33220AStreamInterface(StreamAdapter):
             new_output = ["OFF", "ON"].index(new_output)
         self._device.output = new_output
 
-
+    def get_idn(self):
+        return self._device.idn
 
 
 ###################
