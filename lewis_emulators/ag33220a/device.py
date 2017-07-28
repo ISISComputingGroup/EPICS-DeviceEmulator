d26fa4a5c86983546be20aa9d9ae3eca059ce3d3
Jack Draper <drapj002@gmail.com>
2017-07-28 16:43:26 +0100

    Placed complex functions into device.py and volt high/low, amplitude and offset now are affected by one another.

--- lewis_emulators/ag33220a/device.py
+++ lewis_emulators/ag33220a/device.py
@@ -3,17 +3,110 @@ from lewis.devices import Device
 
 class SimulatedAG33220A(Device):
     make = "Agilent Technologies,33220A"
-    firmwareRevNum = "MY44033103,2.02"
-    bootKernelRevisionNumber = "2.02"
-    asicRevNum = "22"
-    printedCircuitBoardRevNum = "2"
-    idn = make+"-"+firmwareRevNum+"-"+bootKernelRevisionNumber+"-"+asicRevNum+"-"+printedCircuitBoardRevNum
-    voltage = 0.1
+    firmware_rev_num = "MY44033103,2.02"
+    boot_kernel_revision_number = "2.02"
+    asic_rev_num = "22"
+    printed_circuit_board_rev_num = "2"
+    idn = make+"-"+firmware_rev_num+"-"+boot_kernel_revision_number+"-"+asic_rev_num+"-"+printed_circuit_board_rev_num
+    amplitude = 0.1
     frequency = 1000
     offset = 0
     units = "VPP"
     function = "SIN"
     output = 0
-    voltageHigh = 0.05
-    voltageLow = -0.05
+    voltage_high = 0.05
+    voltage_low = -0.05
+    amplitude_lower_bound = 0.01
+    amplitude_upper_bound = 10
+    offset_lower_bound = -4.995
+    offset_upper_bound = 4.995
+    voltage_lower_bound = -5
+    voltage_upper_bound = 5
+    voltage_low_lower_bound = -5
+    voltage_low_upper_bound = 4.99
+    voltage_high_lower_bound = -4.99
+    voltage_high_upper_bound = 5
+    voltage_precision = 0.01
+
+
+
+
+    def limit_or_min_max(self, value, minimum, maximum):
+        try:
+            return self.limit(float(value), minimum, maximum)
+        except:
+            return {"MIN": minimum, "MAX": maximum}[value]
+
+    # If the value is above or below the upper or lower bound
+    # then the upper or lower bound will be returned respectively
+    # otherwise the value is returned
+    def limit(self, value, lower_bound, upper_bound):
+        if value >= upper_bound:
+            return upper_bound
+        elif value < lower_bound:
+            return lower_bound
+        else:
+            return value
+
+    def set_new_amplitude(self, new_amplitude):
+        new_amplitude = self.limit_or_min_max(new_amplitude, self.amplitude_lower_bound, self.amplitude_upper_bound)
+        if 0.5 * new_amplitude + self.offset > self.voltage_upper_bound or self.offset - 0.5 * new_amplitude < self.voltage_lower_bound:
+            if 0.5 * new_amplitude + self.offset > self.voltage_upper_bound:
+                offset_difference = 0.5 * new_amplitude + self.offset - self.voltage_upper_bound
+            elif self.offset - 0.5 * new_amplitude < self.voltage_lower_bound:
+                offset_difference = -0.5 * new_amplitude + self.offset + self.voltage_upper_bound
+            self.offset -= offset_difference
+            self.voltage_high -= offset_difference
+            self.voltage_low -= offset_difference
+        else:
+            self.update_volt_high_and_low(new_amplitude, self.offset)
+        self.amplitude = new_amplitude
+
+    def set_new_frequency(self, new_frequency):
+        self.frequency = self.limit_or_min_max(new_frequency, self.frequency_lower_bound(), self.frequency_upper_bound())
+
+    def set_new_voltage_high(self,new_voltage_high):
+        new_voltage_high = self.limit(new_voltage_high, self.voltage_high_lower_bound, self.voltage_high_upper_bound)
+        if new_voltage_high <= self.voltage_low:
+            self.voltage_low = self.limit(new_voltage_high - self.voltage_precision, self.voltage_low_lower_bound, new_voltage_high)
+        self.update_volt_and_offs(self.voltage_low, new_voltage_high)
+
+    def set_new_voltage_low(self,new_voltage_low):
+        new_voltage_low = self.limit(new_voltage_low, self.voltage_low_lower_bound, self.voltage_low_upper_bound)
+        if new_voltage_low >= self.voltage_high:
+            self.set_voltage_high(self.limit(new_voltage_low + self.voltage_precision, new_voltage_low, self.voltage_high_upper_bound))
+        self.update_volt_and_offs(new_voltage_low, self.voltage_high)
+
+    def update_volt_and_offs(self, new_low, new_high):
+        self.voltage_high = new_high
+        self.voltage_low = new_low
+        self.amplitude = self.voltage_high-self.voltage_low
+        self.offset = (self.voltage_high+self.voltage_low)/2
+
+    def set_offs_and_update_voltage(self, new_offset):
+        new_offset = self.limit_or_min_max(new_offset, self.offset_lower_bound, self.offset_upper_bound)
+        if new_offset+self.voltage_high > self.voltage_upper_bound:
+            self.amplitude = 2/(self.voltage_upper_bound-new_offset)
+            self.voltage_high = self.voltage_upper_bound
+            self.voltage_low = self.voltage_upper_bound - self.amplitude
+        elif new_offset+self.voltage_low < self.voltage_lower_bound:
+            self.amplitude = 2/(self.voltage_lower_bound-new_offset)
+            self.voltage_low = self.voltage_lower_bound
+            self.voltage_high = self.voltage_lower_bound + self.amplitude
+        else:
+            self.update_volt_high_and_low(self.amplitude, new_offset)
+        self.offset = new_offset
+
+    def update_volt_high_and_low(self, new_volt, new_offs):
+        self.offset = new_offs
+        self.amplitude = new_volt
+        self.voltage_high = new_offs + new_volt / 2
+        self.voltage_low = new_offs - new_volt / 2
+
+    def frequency_lower_bound(self):
+        return {"SIN": 10 ** -6, "SQU": 10 ** -6, "RAMP": 10 ** -6, "PULS": 5 * 10 ** -4, "NOIS": 1 * 10 ** -6, "USER": 10 ** -6}[self.function]
+
+    def frequency_upper_bound(self):
+        return {"SIN": 2 * 10 ** 7, "SQU": 2 * 10 ** 7, "RAMP": 2 * 10 ** 5, "PULS": 5 * 10 ** 6, "NOIS": 2 * 10 ** 7,"USER": 6 * 10 ** 6}[self.function]
+
     pass
--- lewis_emulators/ag33220a/interfaces/stream_interface.py
+++ lewis_emulators/ag33220a/interfaces/stream_interface.py
@@ -1,15 +1,16 @@
 from lewis.adapters.stream import StreamAdapter, Cmd
+import traceback
 
 
 class AG33220AStreamInterface(StreamAdapter):
 
     commands = {
-        Cmd("get_voltage", "^VOLT\?$"),
-        Cmd("set_voltage", "^VOLT " + "([\-0-9.]+|MAX|MIN)$", argument_mappings=[str]),
-        Cmd("get_freq", "^FREQ\?$"),
-        Cmd("set_freq", "^FREQ " + "([\-0-9.]+|MAX|MIN)$", argument_mappings=[str]),
+        Cmd("get_amplitude", "^VOLT\?$"),
+        Cmd("set_amplitude", "^VOLT " + "([\-0-9.]+|MAX|MIN)$", argument_mappings=[str]),
+        Cmd("get_frequency", "^FREQ\?$"),
+        Cmd("set_frequency", "^FREQ " + "([\-0-9.]+|MAX|MIN)$", argument_mappings=[str]),
         Cmd("get_offset", "^VOLT:OFFS\?$"),
-        Cmd("set_offset", "^VOLT:OFFS " + "([\-0-9.]+)$", argument_mappings=[float]),
+        Cmd("set_offset", "^VOLT:OFFS " + "([\-0-9.]+|MAX|MIN)$", argument_mappings=[str]),
         Cmd("get_units", "^VOLT:UNIT\?$"),
         Cmd("set_units", "^VOLT:UNIT " + "(VPP|VRMS|DBM)$", argument_mappings=[str]),
         Cmd("get_function", "^FUNC\?$"),
@@ -18,55 +19,37 @@ class AG33220AStreamInterface(StreamAdapter):
         Cmd("set_output", "^OUTP " + "(0|1|ON|OFF)$", argument_mappings=[str]),
         Cmd("get_idn", "^\*IDN\?$"),
         Cmd("get_voltage_high", "^VOLT:HIGH\?$"),
-        Cmd("set_voltage_high", "^VOLT:HIGH" + "([\-0-9.]+)$", argument_mappings=[float]),
+        Cmd("set_voltage_high", "^VOLT:HIGH " + "([\-0-9.]+)$", argument_mappings=[float]),
         Cmd("get_voltage_low", "^VOLT:LOW\?$"),
-        Cmd("set_voltage_low", "^VOLT:LOW" + "([\-0-9.]+)$", argument_mappings=[float]),
+        Cmd("set_voltage_low", "^VOLT:LOW " + "([\-0-9.]+)$", argument_mappings=[float]),
     }
 
     in_terminator = "\n"    # \r\n for putty
     out_terminator = "\n"
 
-    # Takes in a value and returns a value in the form of x.xxxxxxxxxxxxxEYY
+
+    # Takes in a value and returns a value in the form of x.xxx0000000000Eyy
     def float_output(self, value):
         value = float('%s' % float('%.4g' % float(value)))
         return "{:+.13E}".format(value)
 
-    # If the value is above or below the upper or lower bound
-    # then the upper or lower bound will be returned respectively
-    # otherwise the value is returned
-    def limit(self,value,upper_bound,lower_bound):
-        if value >= upper_bound:
-            return upper_bound
-        elif value < lower_bound:
-            return lower_bound
-        else:
-            return value
+    def get_amplitude(self):
+        return self.float_output(self._device.amplitude)
 
-    def get_voltage(self):
-        return self.float_output(self._device.voltage)
+    def set_amplitude(self, new_amplitude):
+        self._device.set_new_amplitude(new_amplitude)
 
-    def set_voltage(self, new_voltage):
-        try:
-            self._device.voltage = self.limit(float(new_voltage), 10, 0.01)
-        except:
-            self._device.voltage = {"MIN": 0.01, "MAX": 10}[new_voltage]
-
-    def get_freq(self):
+    def get_frequency(self):
         return self.float_output(self._device.frequency)
 
-    def set_freq(self, new_frequency):
-        lower_bound = {"SIN": 10**-6, "SQU": 10**-6, "RAMP": 10**-6, "PULS": 5*10**-4, "NOIS": 1*10**-6, "USER": 10**-6}[self._device.function]
-        upper_bound = {"SIN": 2*10**7, "SQU": 2*10**7, "RAMP": 2*10**5, "PULS": 5*10**6, "NOIS": 2*10**7, "USER": 6*10**6}[self._device.function]
-        try:
-            self._device.frequency = self.limit(float(new_frequency), upper_bound, lower_bound)
-        except:
-            self._device.frequency = {"MIN": lower_bound, "MAX": upper_bound}[new_frequency]
+    def set_frequency(self, new_frequency):
+        self._device.set_new_frequency(new_frequency)
 
     def get_offset(self):
         return self.float_output(self._device.offset)
 
     def set_offset(self, new_offset):
-        self._device.offset = new_offset
+        self._device.set_offs_and_update_voltage(new_offset)
 
     def get_units(self):
         return self._device.units
@@ -78,10 +61,8 @@ class AG33220AStreamInterface(StreamAdapter):
         return self._device.function
 
     def set_function(self, new_function):
-        print(new_function,"\n",self._device.function)
         self._device.function = new_function
-        self.set_freq(self._device.frequency)
-        print(new_function, "\n\n", self._device.function)
+        self.set_frequency(self._device.frequency)
 
     def get_output(self):
         return self._device.output
@@ -97,27 +78,16 @@ class AG33220AStreamInterface(StreamAdapter):
         return self._device.idn
 
     def get_voltage_high(self):
-        return self.float_output(self._device.voltageHigh)
+        return self.float_output(self._device.voltage_high)
 
     def set_voltage_high(self, new_voltage_high):
-        self._device.voltageHigh = new_voltage_high
+        self._device.set_new_voltage_high(new_voltage_high)
 
     def get_voltage_low(self):
-        return self.float_output(self._device.voltageLow)
+        return self.float_output(self._device.voltage_low)
 
     def set_voltage_low(self, new_voltage_low):
-        self._device.voltageLow = new_voltage_low
+        self._device.set_new_voltage_low(new_voltage_low)
 
     def handle_error(self, request, error):
-        print(str(error))
-
-###################
-#    def get_reading(self):
-#        rand = random.random() * 100.0
-#        min_width = 10
-#        data_str = "READ:" + "{:0.3f}".format(rand).ljust(min_width) + ";" + str(self._device.setpoint_mode)
-#        return self.create_response(READING_COMM + "  ", data=data_str)
-#
-#    def handle_error(self, request, error):
-#        print "An error occurred at request " + repr(request) + ": " + repr(error)
-####################
+        print traceback.format_exc()
