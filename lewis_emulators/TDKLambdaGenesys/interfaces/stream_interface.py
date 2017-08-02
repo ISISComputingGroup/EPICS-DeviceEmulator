781d3130b54aa8b022ee9405ea1b8de67eb3c335
esouthren <eilidhsouthren@gmail.com>
2017-08-02 15:35:21 +0100

    Update stream_interface.py
    Line formatting

--- lewis_emulators/TDKLambdaGenesys/interfaces/stream_interface.py
+++ lewis_emulators/TDKLambdaGenesys/interfaces/stream_interface.py
@@ -1,24 +1,16 @@
 from lewis.adapters.stream import StreamAdapter, Cmd
 from lewis.core.logging import has_log
-from utils.command_builder import CmdBuilder
+frin utils.command_builder import CmdBuilder
 
 class TDKLambdaGenesysStreamInterface(StreamAdapter):
 
     commands = {
-
         CmdBuilder("write_voltage").escape("VOLT ").float().build(),
         CmdBuilder("read_setpoint_voltage").escape("PV?").build(),
         CmdBuilder("read_voltage").escape("MV?").build(),
-
         CmdBuilder("write_current").escape("PC").float().build(),
         CmdBuilder("read_setpoint_current").escape("PC?").build(),
         CmdBuilder("read_current").escape("MC?").build(),
-
-        #CmdBuilder("remote").escape("RMT 1").build(),
-        #CmdBuilder("reset").escape("RST").build(),
-        #CmdBuilder("write_powerstate").escape("OFF|ON").build(),
-        #CmdBuilder("write_powerstate").escape("OUT?"),
-        #CmdBuilder("write_address").escape("ADR").float().build(),
     }
 
     in_terminator = "\r"
