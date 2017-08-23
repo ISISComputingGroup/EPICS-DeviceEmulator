f3acfaf7b88ca658d5e3d24cdd8f6f42762d21ef
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-08-23 18:25:34 +0100

    Fix bug

--- lewis_emulators/instron_stress_rig/quarter_cycle_event_detector.py
+++ lewis_emulators/instron_stress_rig/quarter_cycle_event_detector.py
@@ -19,7 +19,7 @@ class QuarterCycleEventDetector(object):
             self.counts += 1
             # This is intentionally == not >=. The counter won't trip if it already exceeds max_counts
             if self.counts == self.max_counts:
-                self.state == QCEDStates.TRIPPED
+                self.state = QCEDStates.TRIPPED
 
     def off(self):
         self.state = QCEDStates.OFF
