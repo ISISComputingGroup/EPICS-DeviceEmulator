a363d6953d648cef2ace72903dc4e3ea78cb995a
esouthren <eilidhsouthren@gmail.com>
2017-09-18 11:46:37 +0100

    commit before starting to work on States and everything probably went a bit pete tong

--- lewis_emulators/HFMAGPSU/device.py
+++ lewis_emulators/HFMAGPSU/device.py
@@ -5,19 +5,19 @@ from .states import DefaultState
 
 class SimulatedHFMAGPSU(StateMachineDevice):
 
-
     def _initialize_data(self):
-        self._is_output_mode_tesla = False
-        self._is_heater_on = False
-        self._is_paused = False
-        self._direction = 0
-        self._ramp_target = 0
-        self._heater_value = 1.0
-        self._max_target = 5.0
-        self._mid_target = 2.5
-        self._ramp_rate = 10.0
-        self._limit = 10
-        self._log_message = "this is the initial log message"
+        self.is_output_mode_tesla = False
+        self.is_heater_on = True
+        self.is_paused = True
+        self.output = 0
+        self.direction = '-'
+        self.ramp_target = 'ZERO'
+        self.heater_value = 1.0
+        self.max_target = 20.0
+        self.mid_target = 10.0
+        self.ramp_rate = 0.5
+        self.limit = 10
+        self.log_message = "this is the initial log message"
 
     def _get_state_handlers(self):
         return {DefaultState.NAME: DefaultState()}
@@ -27,91 +27,3 @@ class SimulatedHFMAGPSU(StateMachineDevice):
 
     def _get_transition_handlers(self):
         return OrderedDict()
-
-    @property
-    def direction(self):
-        return self._direction
-
-    @property
-    def is_output_mode_tesla(self):
-        return self._is_output_mode_tesla
-
-    @property
-    def ramp_target(self):
-        return self._ramp_target
-
-    @property
-    def is_heater_on(self):
-        return self._is_heater_on
-
-    @property
-    def heater_value(self):
-        return self._heater_value
-
-    @property
-    def max_target(self):
-        return self._max_target
-
-    @property
-    def mid_target(self):
-        return self._mid_target
-
-    @property
-    def ramp_rate(self):
-        return self._ramp_rate
-
-    @property
-    def is_paused(self):
-        return self._is_paused
-
-    @property
-    def limit(self):
-        return self._limit
-
-    @property
-    def log_message(self):
-        return self._log_message
-
-    @direction.setter
-    def direction(self, d):
-        self._direction = d
-
-    @is_output_mode_tesla.setter
-    def is_output_mode_tesla(self, om):
-        self._is_output_mode_tesla = om
-
-    @ramp_target.setter
-    def ramp_target(self, rt):
-        self._ramp_target = rt
-
-    @is_heater_on.setter
-    def is_heater_on(self, hs):
-        self._is_heater_on = hs
-
-    @heater_value.setter
-    def heater_value(self, hv):
-        self._heater_value = hv
-
-    @max_target.setter
-    def max_target(self, mt):
-        self._max_target = mt
-
-    @mid_target.setter
-    def mid_target(self, mt):
-        self._mid_target = mt
-
-    @ramp_rate.setter
-    def ramp_rate(self, rate):
-        self._ramp_rate = rate
-
-    @is_paused.setter
-    def is_paused(self, p):
-        self._is_paused = p
-
-    @limit.setter
-    def limit(self, lim):
-        self._limit = lim
-
-    @log_message.setter
-    def log_message(self, lm):
-        self._log_message = lm
--- lewis_emulators/HFMAGPSU/interfaces/stream_interface.py
+++ lewis_emulators/HFMAGPSU/interfaces/stream_interface.py
@@ -12,12 +12,13 @@ class HFMAGPSUStreamInterface(StreamAdapter):
 
     commands = {
         CmdBuilder("read_direction").escape("GET SIGN").build(),
-        CmdBuilder("read_output_mode").escape("GET O").build(),
-        CmdBuilder("read_ramp_target").escape("R S").build(),
+        CmdBuilder("read_output_mode").escape("T").build(),
+        CmdBuilder("read_output").escape("GET O").build(),
+        CmdBuilder("read_ramp_target").escape("RAMP").build(),
         CmdBuilder("read_heater_status").escape("HEATER").build(),
         CmdBuilder("read_max_target").escape("GET MAX").build(),
         CmdBuilder("read_mid_target").escape("GET MID").build(),
-        CmdBuilder("read_ramp_rate").escape("RAMP").build(),
+        CmdBuilder("read_ramp_rate").escape("GET RATE").build(),
         CmdBuilder("read_limit").escape("H V").build(),
         CmdBuilder("read_pause").escape("P").build(),
         CmdBuilder("read_heater_value").escape("GET H").build(),
@@ -36,14 +37,30 @@ class HFMAGPSUStreamInterface(StreamAdapter):
 
     def _create_log_message(self, pv, value):
         current_time = datetime.now().strftime('%H:%M:%S')
-        self._device.log_message = "{} {}: [{}]".format(current_time, pv, value)
+        self._device.log_message = "{} {}: {}".format(current_time, pv, value)
+
 
     def handle_error(self, request, error):
         self.log.error("Error occurred at " + repr(request) + ": " + repr(error))
         print("Error occurred at " + repr(request) + ": " + repr(error))
 
+    def _get_output_mode_string(self):
+        if self._device.is_output_mode_tesla:
+            return "TESLA"
+        else:
+            return "AMPS"
+
+    def _get_ramp_target_value(self):
+        target = 0
+        if self._device.ramp_target == "MID":
+            target = self._device.mid_target
+        elif self._device.ramp_target == "MAX":
+            target = self._device.max_target
+
+        return target
+
     def read_direction(self):
-        return self._device.direction
+        return "HH.MM.SS CURRENT DIRECTION: {}".format(self._device.direction)
 
     def write_direction(self, direction):
         self._device.direction = direction
@@ -51,90 +68,121 @@ class HFMAGPSUStreamInterface(StreamAdapter):
         return self._device.log_message
 
     def read_output_mode(self):
-        return "TESLA" if self._device.is_output_mode_tesla else "AMPS"
+        mode = "AMPS"
+        if self._device.is_output_mode_tesla:
+            mode = "TESLA"
+        return "HH.MM.SS UNITS: {}".format(mode)
+
+    def read_output(self):
+        # If target is + or - the current output, we are ramping up or down
+        # new output = current output +/- ramp rate
+        if not self._device.is_paused:
+            target = self._get_ramp_target_value()
+            if target >= self._device.output:
+                self._device.output += self._device.ramp_rate
+            elif target < :
+                self._device.output -= self._device.ramp_rate
+        mode = self._get_output_mode_string()
+
+        return "HH.MM.SS OUTPUT: {} {} AT {} VOLTS".format(self._device.output, mode, self._device.heater_value)
 
     def write_output_mode(self, output_mode):
         if output_mode == "TESLA":
             self._device.is_output_mode_tesla = True
-            self._create_log_message("OUTPUT MODE", str(output_mode))
+            self._create_log_message("OUTPUT MODE", output_mode)
         elif output_mode == "AMPS":
             self._device.is_output_mode_tesla = False
-            self._create_log_message("OUTPUT MODE", str(output_mode))
+            self._create_log_message("OUTPUT MODE", output_mode)
         else:
             raise AssertionError("Invalid arguments sent")
         return self._device.log_message
 
     def read_ramp_target(self):
-        return self._device.ramp_target
+        return "HH:MM:SS RAMP TARGET: {}".format(self._device.ramp_target)
 
     def write_ramp_target(self, ramp_target):
         self._device.ramp_target = ramp_target
-        self._create_log_message("RAMP TARGET", str(ramp_target))
+        self._create_log_message("RAMP TARGET", ramp_target)
         return self._device.log_message
 
     def read_ramp_rate(self):
-        return self._device.ramp_rate
+        return "HH:MM:SS RAMP RATE {} A/SEC".format(self._device.ramp_rate)
 
     def write_ramp_rate(self, ramp_rate):
         self._device.ramp_rate = ramp_rate
-        self._create_log_message("RAMP RATE", str(ramp_rate))
+        self._create_log_message("RAMP RATE", ramp_rate)
         return self._device.log_message
 
     def read_heater_status(self):
-        return "ON" if self._device.is_heater_on else "OFF"
+        heater_value = "OFF"
+        if self._device.heater_value:
+            heater_value = "ON"
+        return "HH:MM:SS HEATER STATUS: {}".format(heater_value)
 
     def write_heater_status(self, heater_status):
         if heater_status == "ON":
             self._device.is_heater_on = True
-            self._create_log_message("HEATER STATUS", str(heater_status))
+            self._create_log_message("HEATER STATUS", heater_status)
         elif heater_status == "OFF":
             self._device.is_heater_on = False
-            self._create_log_message("HEATER STATUS", str(heater_status))
+            self._create_log_message("HEATER STATUS", heater_status)
         else:
             raise AssertionError("Invalid arguments sent")
         return self._device.log_message
 
     def read_pause(self):
-        return "ON" if self._device.is_paused else "OFF"
+        paused = "OFF"
+        if self._device.is_paused:
+            paused = "ON"
+        return "HH:MM:SS PAUSE STATUS: {}".format(paused)
 
     def write_pause(self, paused):
+        mode = self._get_output_mode_string()
+        target = self._get_ramp_target_value()
         if paused == "ON":
             self._device.is_paused = True
-            self._create_log_message("PAUSE STATUS", str(paused))
+            output = "HOLDING ON PAUSE AT {} {}".format(self._device.output, mode)
+            self._create_log_message("RAMP STATUS", output)
         elif paused == "OFF":
             self._device.is_paused = False
-            self._create_log_message("PAUSE STATUS", str(paused))
+            output = "RAMPING FROM {} TO {} {} AT A/SEC".format(self._device.output,
+                                                                target, mode,
+                                                                self._device.ramp_rate)
+            self._create_log_message("RAMP STATUS", output)
         else:
             raise AssertionError("Invalid arguments sent")
 
         return self._device.log_message
 
     def read_heater_value(self):
-        return self._device.heater_value
+        return "HH:MM:SS HEATER OUTPUT: {} VOLTS".format(self._device.heater_value)
 
     def write_heater_value(self, heater_value):
         self._device.heater_value = heater_value
-        self._create_log_message("HEATER OUTPUT", str(heater_value))
+        self._create_log_message("HEATER OUTPUT", heater_value)
         return self._device.log_message
 
     def read_max_target(self):
-        return self._device.max_target
+        mode = self._get_output_mode_string()
+        return "HH:MM:SS MAX SETTING: {} {}".format(self._device.max_target, mode)
+
 
     def write_max_target(self, max_target):
         self._device.max_target = max_target
-        self._create_log_message("MAX SETTING", str(max_target))
+        self._create_log_message("MAX SETTING", max_target)
         return self._device.log_message
 
     def read_mid_target(self):
-        return self._device.mid_target
+        mode = self._get_output_mode_string()
+        return "HH:MM:SS MID SETTING: {} {}".format(self._device.mid_target, mode)
 
     def write_mid_target(self, mid_target):
         self._device.mid_target = mid_target
-        self._create_log_message("MID SETTING", str(mid_target))
+        self._create_log_message("MID SETTING", mid_target)
         return self._device.log_message
 
     def read_limit(self):
-        return self._device.limit
+        return "HH:MM:SS VOLTAGE LIMIT: {} VOLTS".format(self._device.limit)
 
     def write_limit(self, limit):
         self._device.limit = limit
