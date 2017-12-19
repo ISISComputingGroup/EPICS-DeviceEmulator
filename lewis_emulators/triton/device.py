100a2abc1e08500bc96d0efa6213143e5021eecf
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-12-19 12:40:56 +0000

    PID controls working

--- lewis_emulators/triton/device.py
+++ lewis_emulators/triton/device.py
@@ -3,13 +3,20 @@ from states import DefaultState
 from lewis.devices import StateMachineDevice
 
 
+SUBSYSTEM_NAMES = {
+        "mixing chamber": "mix_chamber_name",
+    }
+
+
 class SimulatedTriton(StateMachineDevice):
 
     def _initialize_data(self):
         """
         Initialize all of the device's attributes.
         """
-        pass
+        self.p = 0
+        self.i = 0
+        self.d = 0
 
     def _get_state_handlers(self):
         return {
@@ -23,3 +30,20 @@ class SimulatedTriton(StateMachineDevice):
         return OrderedDict([
         ])
 
+    def set_p(self, value):
+        self.p = value
+
+    def set_i(self, value):
+        self.i = value
+
+    def set_d(self, value):
+        self.d = value
+
+    def get_p(self):
+        return self.p
+
+    def get_i(self):
+        return self.i
+
+    def get_d(self):
+        return self.d
--- lewis_emulators/triton/interfaces/stream_interface.py
+++ lewis_emulators/triton/interfaces/stream_interface.py
@@ -1,15 +1,48 @@
 from lewis.adapters.stream import StreamInterface, Cmd
+from lewis_emulators.utils.command_builder import CmdBuilder
+from lewis_emulators.triton.device import SUBSYSTEM_NAMES
 
 
 class TritonStreamInterface(StreamInterface):
 
     # Commands that we expect via serial during normal operation
     commands = {
-        Cmd("get_mc_uid", "^READ:SYS:DR:CHAN:MC$"),  # Catch-all command for debugging
+        CmdBuilder("get_mc_uid").escape("READ:SYS:DR:CHAN:MC").build(),
+
+        # PID setpoints
+        CmdBuilder("set_p").escape("SET:DEV:{}:TEMP:LOOP:P:".format(SUBSYSTEM_NAMES["mixing chamber"])).float().build(),
+        CmdBuilder("set_i").escape("SET:DEV:{}:TEMP:LOOP:I:".format(SUBSYSTEM_NAMES["mixing chamber"])).float().build(),
+        CmdBuilder("set_d").escape("SET:DEV:{}:TEMP:LOOP:D:".format(SUBSYSTEM_NAMES["mixing chamber"])).float().build(),
+
+        # PID readbacks
+        CmdBuilder("get_p").escape("READ:DEV:{}:TEMP:LOOP:P".format(SUBSYSTEM_NAMES["mixing chamber"])).build(),
+        CmdBuilder("get_i").escape("READ:DEV:{}:TEMP:LOOP:I".format(SUBSYSTEM_NAMES["mixing chamber"])).build(),
+        CmdBuilder("get_d").escape("READ:DEV:{}:TEMP:LOOP:D".format(SUBSYSTEM_NAMES["mixing chamber"])).build(),
     }
 
     in_terminator = "\r\n"
     out_terminator = "\r\n"
 
     def get_mc_uid(self):
-        return "STAT:SYS:DR:CHAN:MC:{}".format("mix_chamber_name")
+        return "STAT:SYS:DR:CHAN:MC:{}".format(SUBSYSTEM_NAMES["mixing chamber"])
+
+    def set_p(self, value):
+        self.device.set_p(value)
+        return "ok"
+
+    def set_i(self, value):
+        self.device.set_i(value)
+        return "ok"
+
+    def set_d(self, value):
+        self.device.set_d(value)
+        return "ok"
+
+    def get_p(self):
+        return "STAT:DEV:{}:TEMP:LOOP:P:{}".format(SUBSYSTEM_NAMES["mixing chamber"], self.device.get_p())
+
+    def get_i(self):
+        return "STAT:DEV:{}:TEMP:LOOP:I:{}".format(SUBSYSTEM_NAMES["mixing chamber"], self.device.get_i())
+
+    def get_d(self):
+        return "STAT:DEV:{}:TEMP:LOOP:D:{}".format(SUBSYSTEM_NAMES["mixing chamber"], self.device.get_d())
