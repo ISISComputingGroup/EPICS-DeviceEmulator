abec274bd0b81b629bec2cb70ab23f002d9d7f79
Adrian Potter <Adrian.Potter@tessella.com>
2018-02-12 11:48:51 +0000

    Add ISOBUS channel to command

--- lewis_emulators/ilm200/interfaces/stream_interface.py
+++ lewis_emulators/ilm200/interfaces/stream_interface.py
@@ -1,23 +1,10 @@
-from lewis.adapters.stream import StreamInterface, Cmd
+from lewis.adapters.stream import StreamInterface
 from lewis.core.logging import has_log
 from lewis_emulators.utils.command_builder import CmdBuilder
-from ..channel import Channel
 
 
 @has_log
 class Ilm200StreamInterface(StreamInterface):
-
-    # Commands that we expect via serial during normal operation
-    commands = {
-
-        Cmd("get_version", "^V$"),
-
-        Cmd("get_status", "^X$"),
-
-        Cmd("get_level", "^R1$"),
-        Cmd("set_rate_fast", "^T1$"),
-        Cmd("set_rate_slow", "^S2$"),
-    }
 
     in_terminator = "\r"
     out_terminator = "\r"
@@ -25,31 +12,33 @@ class Ilm200StreamInterface(StreamInterface):
     def __init__(self):
 
         super(Ilm200StreamInterface, self).__init__()
+        # All commands preceded by "@i" where i is the ISOBUS address. Has no impact on emulator so is ignored.
         self.commands = {
-            CmdBuilder(self.get_version).escape("V").build(),
-            CmdBuilder(self.get_status).escape("X").build(),
-            CmdBuilder(self.get_level).escape("R").int().build(),
-            CmdBuilder(self.set_rate_slow).escape("S").int().build(),
-            CmdBuilder(self.set_rate_fast).escape("T").int().build(),
+            CmdBuilder(Ilm200StreamInterface.get_version).escape("@").int().escape("V").build(),
+            CmdBuilder(self.get_status).escape("@").int().escape("X").build(),
+            CmdBuilder(self.get_level).escape("@").int().escape("R").int().build(),
+            CmdBuilder(self.set_rate_slow).escape("@").int().escape("S").int().build(),
+            CmdBuilder(self.set_rate_fast).escape("@").int().escape("T").int().build(),
         }
 
     def handle_error(self, request, error):
         print("An error occurred at request " + repr(request) + ": " + repr(error))
         return str(error)
 
-    def get_version(self):
+    @staticmethod
+    def get_version(_):
         return "IBEX Emulator"
 
-    def set_rate_slow(self, channel):
+    def set_rate_slow(self, _, channel):
         self._device.set_fill_rate(channel=int(channel), fast=False)
 
-    def set_rate_fast(self, channel):
+    def set_rate_fast(self, _, channel):
         self._device.set_fill_rate(channel=int(channel), fast=True)
 
-    def get_level(self, channel):
+    def get_level(self, _, channel):
         return self._device.get_level(channel=int(channel))
 
-    def _get_status(self, channel_number):
+    def _get_channel_status(self, channel_number):
         channel = self._device.channels[channel_number]
         bits = (
             channel.has_helium_current(),
@@ -63,16 +52,18 @@ class Ilm200StreamInterface(StreamInterface):
         )
         return sum([int(set_it)*2**bit for bit, set_it in enumerate(bits)])
 
-    def _get_logic_status(self):
+    @staticmethod
+    def _get_logic_status():
         return 0
 
-    def get_status(self):
+    def get_status(self, _):
         d = self._device
         status_string = "X{ch1_type:01d}{ch2_type:01d}{ch3_type:01d}S{ch1_status:02d}{ch2_status:02d}"\
-               "{ch3_status:02d}R{logic_status:02d}".format(
-                   ch1_type=d.get_cryo_type(1), ch2_type=d.get_cryo_type(2), ch3_type=d.get_cryo_type(3),
-                   ch1_status=self._get_status(1), ch2_status=self._get_status(2), ch3_status=self._get_status(3),
-                   logic_status=self._get_logic_status()
-               )
+                        "{ch3_status:02d}R{logic_status:02d}".format(
+                            ch1_type=d.get_cryo_type(1), ch2_type=d.get_cryo_type(2), ch3_type=d.get_cryo_type(3),
+                            ch1_status=self._get_channel_status(1), ch2_status=self._get_channel_status(2),
+                            ch3_status=self._get_channel_status(3),
+                            logic_status=Ilm200StreamInterface._get_logic_status()
+                        )
         print("STATUS: " + status_string)
         return status_string
