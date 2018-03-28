01ae47fd232841753bdb428180cda31cee1e24dd
John-Holt-Tessella <John.Holt@tessella.com>
2018-03-28 16:33:52 +0100

    Add template emulator

---
+++ lewis_emulators/cryvalve/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedCryvalve
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedCryvalve']
---
+++ lewis_emulators/cryvalve/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import CryvalveStreamInterface
+
+__all__ = ['CryvalveStreamInterface']
