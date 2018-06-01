e782e15fe9e9c57fa2a7599b1009aa08b2b6ea5c
Jeremy Spencer <jeremy.spencer@stfc.ac.uk>
2018-06-01 17:32:35 +0100

    Add template emulator

---
+++ lewis_emulators/testgen3/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedTestgen3
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedTestgen3']
---
+++ lewis_emulators/testgen3/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import Testgen3StreamInterface
+
+__all__ = ['Testgen3StreamInterface']
