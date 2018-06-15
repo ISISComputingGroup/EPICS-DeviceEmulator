f85fad2d8445c50d8524537411e8a38ae7368f3f
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-06-15 15:29:14 +0100

    Added run and run? commands to emulator.

--- lewis_emulators/sp2xx/device.py
+++ lewis_emulators/sp2xx/device.py
@@ -1,6 +1,14 @@
 from collections import OrderedDict
 from states import DefaultState
 from lewis.devices import StateMachineDevice
+from enum import Enum
+
+
+class RunStatus(Enum):
+    Stopped = 0
+    Infusing = 1
+    Withdrawing = 2
+
 
 
 class SimulatedSp2XX(StateMachineDevice):
@@ -9,17 +17,58 @@ class SimulatedSp2XX(StateMachineDevice):
         """
         Initialize all of the device's attributes.
         """
-        pass
+        self.running_status = RunStatus.Stopped
+        self.connect()
 
-    def _get_state_handlers(self):
+    @staticmethod
+    def _get_state_handlers():
         return {
             'default': DefaultState(),
         }
 
-    def _get_initial_state(self):
+    @staticmethod
+    def _get_initial_state():
         return 'default'
 
-    def _get_transition_handlers(self):
+    @staticmethod
+    def _get_transition_handlers():
         return OrderedDict([
         ])
+
+    def set_running_status_via_the_back_door(self, status):
+        """
+        Sets running direction as an Enum to infusion or withdrawing. Called only via the backdoor.
+        Args:
+            direction: "infusion" or "withdrawing".
+
+        Returns:
+            None
+        """
+        self.running_status = RunStatus[status]
+
+    @property
+    def connected(self):
+        """
+        Returns True if the device is connected and False if disconnected.
+        """
+        return self._connected
+
+    def connect(self):
+        """
+        Connects the device.
+
+        Returns:
+            None
+        """
+        self._connected = True
+
+    def discconnect(self):
+        """
+        Disconnects the device.
+
+        Returns:
+            None
+        """
+        self._connected = False
+
 
--- lewis_emulators/sp2xx/interfaces/stream_interface.py
+++ lewis_emulators/sp2xx/interfaces/stream_interface.py
@@ -1,12 +1,84 @@
 from lewis.adapters.stream import StreamInterface, Cmd
+from lewis.core.logging import has_log
+
+from lewis_emulators.utils.command_builder import CmdBuilder
+from lewis_emulators.utils.if_connected import if_connected
+from ..device import RunStatus
 
 
+@has_log
 class Sp2XXStreamInterface(StreamInterface):
+    """
+    Stream interface for the serial port.
+    """
 
     # Commands that we expect via serial during normal operation
     commands = {
-        Cmd("catch_all", "^#9.*$"),  # Catch-all command for debugging
+        CmdBuilder("set_run_status").escape("run").eos().build(),
+        CmdBuilder("get_run_status").escape("run?").eos().build()
     }
 
-    def catch_all(self):
-        pass
+    in_terminator = "\r\n"
+    out_terminator = ""
+
+    _return = "\r\n"
+    Infusing = "{}>".format(_return)
+    Withdrawing = "{}<".format(_return)
+    Stopped = "{}:".format(_return)
+
+    def handle_error(self, request, error):
+        """
+        Prints an error message if a command is not recognised.
+
+        Args:
+            request : Request.
+            error: The error that has occurred.
+        Returns:
+            None.
+        """
+        self.log.error("An error occurred at request " + repr(request) + ": " + repr(error))
+
+        print("An error occurred at request {}: {}".format(request, error))
+
+    @if_connected
+    def set_run_status(self):
+        """
+        Starts the device running to present settings if it is not running.
+
+        Returns:
+            ">" : If device's run direction is in infusion mode.
+            "<" : If device's run direction is in withdrawing mode.
+        """
+        if self._device.running_status != RunStatus.Stopped:
+            if self._device.running_status == RunStatus.Infusing:
+                return self.Infusing
+            elif self._device.running_status == RunStatus.Withdrawing:
+                return self.Withdrawing
+            else:
+                print("An error occurred when trying to run the device. The device's running direction \
+                    is {} and the running state is {}.".format(
+                    self._device.running_direction, self._device.running))
+
+    @if_connected
+    def get_run_status(self):
+        """
+        Gets the run status of the pump.
+
+        Returns:
+            ":" : If the device is not running.
+            ">" : If device's run direction is in infusion mode.
+            "<" : If device's run direction is in withdrawing mode.
+        """
+        if self._device.running_status == RunStatus.Infusing:
+            return self.Infusing
+        elif self._device.running_status == RunStatus.Withdrawing:
+            return self.Withdrawing
+        elif self._device.running_status == RunStatus.Stopped:
+            return self.Stopped
+        else:
+            print("""An error occurred when trying to run the device
+                  "The device's running direction is {} and the running state is {}""".format(
+                    self._device.running_direction, self._device.running))
+
+
+
