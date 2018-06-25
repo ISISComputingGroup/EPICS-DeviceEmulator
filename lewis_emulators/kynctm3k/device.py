6f0812d8b4946fbcbe7205c775dddd3b57750cf8
Alistair McGann <alistair.mcgann@tessella.com>
2018-06-25 11:18:14 +0100

    Added an emulator function to generate an output string

--- lewis_emulators/kynctm3k/device.py
+++ lewis_emulators/kynctm3k/device.py
@@ -9,6 +9,9 @@ class SimulatedKynctm3K(StateMachineDevice):
         """
         Initialize all of the device's attributes.
         """
+        self.OUT_values = None
+        self.program = None
+
         pass
 
     def _get_state_handlers(self):
@@ -23,3 +26,23 @@ class SimulatedKynctm3K(StateMachineDevice):
         return OrderedDict([
         ])
 
+    def format_output_data(self):
+        """
+        Formats a data output string given the current measurement values and program.
+
+        Returns:
+            formatted_data: String containing the measurement values for the current program
+
+        """
+
+        if self.program is None or self.OUT_values is None:
+            return None
+        else:
+            channel_strings = []
+            for channel, program_switch in enumerate(self.program):
+                if program_switch == "1":
+                    channel_strings.append("TG,{:02d},{:+06.3f}\r".format(channel, self.OUT_values[channel]))
+                else:
+                    continue
+
+        return ''.join(channel_strings)
--- lewis_emulators/kynctm3k/interfaces/stream_interface.py
+++ lewis_emulators/kynctm3k/interfaces/stream_interface.py
@@ -5,11 +5,15 @@ class Kynctm3KStreamInterface(StreamInterface):
 
     # Commands that we expect via serial during normal operation
     commands = {
+        Cmd("return_data", "MA$"),
         Cmd("catch_all", "^.*$"),  # Catch-all command for debugging
     }
 
     in_terminator = "\r"
     out_terminator = "\r"
+
+    def return_data(self):
+        return self._device.format_output_data()
 
     def catch_all(self):
         return "Hello, world!"
