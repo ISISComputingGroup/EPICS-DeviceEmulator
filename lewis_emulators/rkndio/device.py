9adddc1ead06991ac2312af23cd19734a3c5a1be
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-06-28 10:51:02 +0100

    Added IDN and connected property.

--- lewis_emulators/rkndio/device.py
+++ lewis_emulators/rkndio/device.py
@@ -9,7 +9,8 @@ class SimulatedRkndio(StateMachineDevice):
         """
         Initialize all of the device's attributes.
         """
-        pass
+        self._idn = "RIKENFE Prototype v1.0"
+        self._connected = True
 
     def _get_state_handlers(self):
         return {
@@ -22,4 +23,36 @@ class SimulatedRkndio(StateMachineDevice):
     def _get_transition_handlers(self):
         return OrderedDict([
         ])
+
+    @property
+    def idn(self):
+        """
+        Returns the IDN of the device.
+
+        Returns:
+            (string): The IDN of the device.
+        """
+        return self._idn
+
+    @property
+    def connected(self):
+        return self._connected
+
+    def connect(self):
+        """
+        Connects the device.
+
+        Returns:
+            None
+        """
+        self._connected = True
+
+    def disconnect(self):
+        """
+        Disconnects the device.
+
+        Returns:
+            Nome
+        """
+        self._connected = False
 
--- lewis_emulators/rkndio/interfaces/stream_interface.py
+++ lewis_emulators/rkndio/interfaces/stream_interface.py
@@ -1,12 +1,32 @@
-from lewis.adapters.stream import StreamInterface, Cmd
+from lewis.adapters.stream import StreamInterface
+from lewis_emulators.utils.command_builder import CmdBuilder
+from lewis_emulators.utils.replies import conditional_reply
 
 
 class RkndioStreamInterface(StreamInterface):
 
     # Commands that we expect via serial during normal operation
     commands = {
-        Cmd("catch_all", "^#9.*$"),  # Catch-all command for debugging
+        CmdBuilder("get_idn").escape("*IDN?").eos().build()
     }
 
-    def catch_all(self):
-        pass
+    in_terminator = "\r\n"
+    out_terminator = "\r\n"
+
+    def handle_error(self, request, error):
+        """
+        Prints an error message if a command is not recognised.
+
+        Args:
+            request : Request.
+            error: The error that has occurred.
+        Returns:
+            None.
+        """
+        self.log.error("An error occurred at request " + repr(request) + ": " + repr(error))
+
+        print("An error occurred at request {}: {}".format(request, error))
+
+    @conditional_reply("connected")
+    def get_idn(self):
+        return self._device.idn
