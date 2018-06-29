7c97055f675d57f78527874b49092824a67531e2
Alistair McGann <alistair.mcgann@tessella.com>
2018-06-29 11:20:43 +0100

    Added a truncate output string flag

--- lewis_emulators/kynctm3k/device.py
+++ lewis_emulators/kynctm3k/device.py
@@ -6,6 +6,24 @@ from lewis.core.logging import has_log
 import random
 
 
+def truncate_if_set(function):
+    """
+    Truncates the decorated function's string output if truncated_output is True
+
+    """
+
+    def wrapper(self, *args, **kwargs):
+        output = function(self, *args, **kwargs)
+
+        if self.truncated_output:
+            output = output[:int(round(len(output) / 2.))]
+
+        return output
+
+    return wrapper
+
+
+@has_log
 class SimulatedKynctm3K(StateMachineDevice):
 
     def _initialize_data(self):
@@ -16,6 +34,7 @@ class SimulatedKynctm3K(StateMachineDevice):
         be in the program, and will not be returned.
         """
         self.OUT_values = None
+        self.truncated_output = False
 
         pass
 
@@ -31,7 +50,34 @@ class SimulatedKynctm3K(StateMachineDevice):
         return OrderedDict([
         ])
 
-    @has_log
+    def parse_status(self, output_setting):
+        """
+        Converts the status for one OUT channel to a formatted string
+        Args:
+            output_setting: String or float. If float, then output is on. If off or out_of_bounds, then a formatted string will be returned
+
+        Returns:
+            OUT_string: String. Contains the measurement value if on, or XXXXXXXX/FFFFFFF as appropriate if off or out of range
+
+        """
+        out_of_range_return = "FFFFFFF"
+        off_return = "XXXXXXXX"
+
+        if output_setting == "off":
+            return off_return
+
+        elif output_setting == "out_of_range":
+            # Add a random sign to the out of range string
+            sign = random.sample(('+', '-'), 1)[0]
+            return sign + out_of_range_return
+
+        elif type(output_setting) is float:
+            return "{:+08.3f}".format(output_setting)
+
+        else:
+            return off_return
+
+    @truncate_if_set
     def format_output_data(self):
         """
         Recalls and formats the measurement values
@@ -41,32 +87,14 @@ class SimulatedKynctm3K(StateMachineDevice):
 
         """
 
-        out_of_range_return = "FFFFFFF"
-        off_return = "XXXXXXXX"
-
         if self.OUT_values is None:
             return None
         else:
             channel_strings = ["MM,1111111111111111", ]
             for channel, output_value in enumerate(self.OUT_values):
-                self.log.info(output_value)
                 # Only return output if the OUT value is in the program
+                channel_strings.append(self.parse_status(output_value))
 
-                if output_value == "off":
-                    channel_strings.append(off_return)
-
-                elif output_value == "out_of_range":
-                    # Add a random sign to the out of range string
-                    sign = random.sample(('+', '-'), 1)[0]
-                    channel_strings.append(sign + out_of_range_return)
-
-                elif type(output_value) is float:
-                    channel_strings.append("{:+08.3f}".format(self.OUT_values[channel]))
-
-                else:
-                    channel_strings.append(off_return)
-
-        self.log.info(self.OUT_values)
         self.log.info(','.join(channel_strings))
 
         return ','.join(channel_strings)
