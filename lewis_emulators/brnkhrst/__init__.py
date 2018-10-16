d732c96a21571565300796f1a879f459956b14e8
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-10-16 10:39:32 +0100

    Add template emulator

---
+++ lewis_emulators/brnkhrst/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedBrnkhrst
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedBrnkhrst']
---
+++ lewis_emulators/brnkhrst/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import BrnkhrstStreamInterface
+
+__all__ = ['BrnkhrstStreamInterface']
