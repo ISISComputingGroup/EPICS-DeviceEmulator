b1c7ce5650c73717d586ad8d06c830f8cf0263f7
aaronlong <aaron.long@stfc.ac.uk>
2018-11-19 16:00:31 +0000

    Functionality for stop, pressure limits, flow rate and status.

--- lewis_emulators/knr1050/device.py
+++ lewis_emulators/knr1050/device.py
@@ -1,7 +1,7 @@
-from collections import OrderedDict
 from states import DefaultState
 from lewis.devices import StateMachineDevice
-
+from collections import OrderedDict
+from enum import Enum
 
 class SimulatedKnr1050(StateMachineDevice):
 
@@ -10,7 +10,29 @@ class SimulatedKnr1050(StateMachineDevice):
         Initialize all of the device's attributes.
         """
         self.connected = True
+
+        self.current_instrument_state = 0
+        self.instrument_state = ('SYS_ST_INITIALIZING',
+                                 'SYS_ST_OFF',
+                                 'SYS_ST_IDLE',
+                                 'SYS_ST_RUN',
+                                 'SYS_ST_HOLD',
+                                 'SYS_ST_PURGE',
+                                 'SYS_ST_STANDBY')
+
         self.is_stopped = False
+        self.keep_last_values = False
+        self.ramp_status = False
+
+        self.pressure_limit_low = 0
+        self.pressure_limit_high = 100
+
+        self.flow_rate = 0
+
+        self.concentration_A = 0
+        self.concentration_B = 0
+        self.concentration_C = 0
+        self.concentration_D = 0
 
 
     def _get_state_handlers(self):
@@ -24,3 +46,6 @@ class SimulatedKnr1050(StateMachineDevice):
     def _get_transition_handlers(self):
         return OrderedDict([
         ])
+
+    def get_instrument_state_str(self, current_instrument_state):
+        return self.instrument_state[current_instrument_state]
--- lewis_emulators/knr1050/interfaces/stream_interface.py
+++ lewis_emulators/knr1050/interfaces/stream_interface.py
@@ -17,7 +17,12 @@ class Knr1050StreamInterface(StreamInterface):
         super(Knr1050StreamInterface, self).__init__()
         # Commands that we expect via serial during normal operation
         self.commands = {
-            CmdBuilder(self.stop).escape('STOP:1,0').build()
+            CmdBuilder(self.get_status).escape("STATUS:").int().eos().build(),
+            CmdBuilder(self.ramp).escape("RAMP:0,").float().escape(",").float().escape(",").float().escape(",").float().escape(",").float().build(),
+            CmdBuilder(self.stop).escape("STOP:1,0").build(),
+            CmdBuilder(self.stop_klv).escape("STOP:2").build(),
+            CmdBuilder(self.get_pressure_limits).escape("PLIM?").build(),
+            CmdBuilder(self.set_pressure_limits).escape("PLIM:").float().escape(",").float().eos().build()
         }
 
 
@@ -30,5 +35,44 @@ class Knr1050StreamInterface(StreamInterface):
         """
         self.log.error("An error occurred at request " + repr(request) + ": " + repr(error))
 
+    def ramp(self, flow_rate, A, B, C, D):
+        """
+        Executes ramp starting from current execution time.
+        Args:
+            flow_rate (float): the flow rate in ul/min
+            A (float): concentration A
+            B (float): concentration B
+            C (float): concentration C
+            D (float): concentration D
+        """
+        self.device.flow_rate = flow_rate
+        self.device.concentration_A = A
+        self.device.concentration_B = B
+        self.device.concentration_C = C
+        self.device.concentration_D = D
+        self.device.ramp_status = True
+
+
     def stop(self):
+        """
+        Stop mode: Stop time table and data acquisition.
+        """
         self.device.is_stopped = True
+
+
+    def stop_klv(self):
+        """
+        Stop mode: Keep last values.
+        """
+        self.device.keep_last_values = True
+        self.device.ramp_status = False
+
+    def get_pressure_limits(self):
+        return "PLIM:{},{}".format(self.device.pressure_limit_low, self.device.pressure_limit_high)
+
+    def set_pressure_limits(self, low, high):
+        self.device.pressure_limit_low = low
+        self.device.pressure_limit_high = high
+
+    def get_status(self, instrument_state):
+        self.device.current_instrument_state = instrument_state
