07550548413abe226382ad9bfdbacc5f722e43fb
Alistair McGann <alistair.mcgann@tessella.com>
2019-01-21 13:48:18 +0000

    Define modbus registers for the different devices

--- lewis_emulators/moxa12xx/interfaces/modbus_interface.py
+++ lewis_emulators/moxa12xx/interfaces/modbus_interface.py
@@ -5,12 +5,36 @@ from lewis.core.logging import has_log
 @has_log
 class Moxa1210ModbusInterface(ModbusInterface):
     """
-    Creates modbus data registers and makes them available to the lewis device.
+    Creates modbus data registers for a Moxa e1210 and makes them available to the lewis device.
     """
+
+    protocol = "MOXA_1210"
 
     di = ModbusBasicDataBank(False, last_addr=0x10)
     co = di
     ir = ModbusBasicDataBank(0)
+    hr = ir
+
+    @ModbusInterface.device.setter
+    def device(self, new_device):
+        """
+        Overrides base implementation to give attached device a reference to self
+        Required to allow communications between the interface and device
+        """
+        ModbusInterface.device.fset(self, new_device)
+        self.device.interface = self
+
+@has_log
+class Moxa1240ModbusInterface(ModbusInterface):
+    """
+    Creates modbus data registers for a Moxa e1240 and makes them available to the lewis device.
+    """
+
+    protocol = "MOXA_1240"
+
+    di = ModbusBasicDataBank(False, last_addr=0x10)
+    co = di
+    ir = ModbusBasicDataBank(10, last_addr=0x80)
     hr = ir
 
     @ModbusInterface.device.setter
