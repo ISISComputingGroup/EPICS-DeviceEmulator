38d3a8583e72a073415afe9407ff60314e66b765
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-02-05 15:18:11 +0000

    Initial emulator

---
+++ lewis_emulators/mezflipr/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedMezFlipr
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedMezFlipr']
---
+++ lewis_emulators/mezflipr/device.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+
+from lewis.devices import StateMachineDevice
+from .states import DefaultState
+
+
+class SimulatedMezFlipr(StateMachineDevice):
+    """
+    Simulated Mezei spin flipper.
+    """
+
+    def _initialize_data(self):
+        """
+        Sets the initial state of the device.
+        """
+        self.connected = True
+
+    def _get_state_handlers(self):
+        """
+        Returns: states and their names
+        """
+        return {DefaultState.NAME: DefaultState()}
+
+    def _get_initial_state(self):
+        """
+        Returns: the name of the initial state
+        """
+        return DefaultState.NAME
+
+    def _get_transition_handlers(self):
+        """
+        Returns: the state transitions
+        """
+        return OrderedDict()
