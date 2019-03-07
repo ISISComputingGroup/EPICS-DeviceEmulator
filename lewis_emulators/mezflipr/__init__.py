5204dc6c94fb43f7520623baff5ff85f2a5e9ad6
Alistair McGann <alistair.mcgann@tessella.com>
2019-03-07 14:29:59 +0000

    Remove accidentally included code

--- lewis_emulators/mezflipr/__init__.py
+++
@@ -1,5 +0,0 @@
-from .device import SimulatedMezFlipr
-from ..lewis_versions import LEWIS_LATEST
-
-framework_version = LEWIS_LATEST
-__all__ = ['SimulatedMezFlipr']
--- lewis_emulators/mezflipr/interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-from .stream_interface import MezfliprStreamInterface
-
-__all__ = ['MezfliprStreamInterface']
