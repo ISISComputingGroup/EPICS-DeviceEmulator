c3facb684bc4fa6fff1dbaa5001554a3bf9626c0
David Keymer <david.keymer@stfc.ac.uk>
2019-04-30 16:17:04 +0100

    Completed support for status and fault codes

--- lewis_emulators/chtobisr/device.py
+++ lewis_emulators/chtobisr/device.py
@@ -3,42 +3,45 @@ from states import DefaultState
 from lewis.devices import StateMachineDevice
 from lewis.core.logging import has_log
 
+
 class SimulatedChtobisr(StateMachineDevice):
+    """
+        Class to simulate Coherent OBIS Laser Remote
+    """
 
     def _initialize_data(self):
         """
-        Initialize all of the device's attributes.
+            Initialize all of the device's attributes.
         """
 
         self.id = "Coherent OBIS Laser Remote - EMULATOR"
-
         self.interlock = "OFF"
-
         self.connected = True
 
-        self.status = 0x00000000
-        self.status.laser_fault = False
-        self.status.laser_emission = False
-        self.status.laser_ready = False
-        self.status.laser_standby = False
-        self.status.cdrh_delay = False
-        self.status.laser_hardware_fault = False
-        self.status.laser_error = False
-        self.status.laser_power_calibration = False
-        self.status.laser_warm_up = False
-        self.status.laser_noise = False
-        self.status.external_operating_mode = False
-        self.status.field_calibration = False
-        self.status.laser_power_voltage = False
+        self.status = {
+            "laser_fault": False,
+            "laser_emission": False,
+            "laser_ready": False,
+            "laser_standby": False,
+            "cdrh_delay": False,
+            "laser_hardware_fault": False,
+            "laser_error": False,
+            "laser_power_calibration": False,
+            "laser_warm_up": False,
+            "laser_noise": False,
+            "external_operating_mode": False,
+            "field_calibration": False,
+            "laser_power_voltage": False,
 
-        self.status.controller_standby = False
-        self.status.controller_interlock = False
-        self.status.controller_enumeration = False
-        self.status.controller_error = False
-        self.status.controller_fault = False
-        self.status.remote_active = False
-        self.status.controller_indicator = False
-
+            "controller_standby": False,
+            "controller_interlock": False,
+            "controller_enumeration": False,
+            "controller_error": False,
+            "controller_fault": False,
+            "remote_active": False,
+            "controller_indicator": False,
+        }
+        
         self.faults = {
             "base_plate_temp_fault": False,
             "diode_temp_fault": False,
@@ -73,6 +76,144 @@ class SimulatedChtobisr(StateMachineDevice):
 
         self._initialize_data()
 
+    def build_status_code(self):
+        """"
+            Builds the device status code
+
+        :return: status code
+        """
+        status_code = 0x00000000
+
+        # Laser specific status bits
+        if self.status["laser_fault"]:
+            status_code += 0x00000001
+        if self.status["laser_emission"]:
+            status_code += 0x00000002
+        if self.status["laser_ready"]:
+            status_code += 0x00000004
+        if self.status["laser_standby"]:
+            status_code += 0x00000008
+        if self.status["cdrh_delay"]:
+            status_code += 0x00000010
+        if self.status["laser_hardware_fault"]:
+            status_code += 0x00000020
+        if self.status["laser_error"]:
+            status_code += 0x00000040
+        if self.status["laser_power_calibration"]:
+            status_code += 0x00000080
+        if self.status["laser_warm_up"]:
+            status_code += 0x00000100
+        if self.status["laser_noise"]:
+            status_code += 0x00000200
+        if self.status["external_operating_mode"]:
+            status_code += 0x00000400
+        if self.status["field_calibration"]:
+            status_code += 0x00000800
+        if self.status["laser_power_voltage"]:
+            status_code += 0x00001000
+
+        # Controller specific status bits
+        if self.status["controller_standby"]:
+            status_code += 0x02000000
+        if self.status["controller_interlock"]:
+            status_code += 0x04000000
+        if self.status["controller_enumeration"]:
+            status_code += 0x08000000
+        if self.status["controller_error"]:
+            status_code += 0x10000000
+        if self.status["controller_fault"]:
+            status_code += 0x20000000
+        if self.status["remote_active"]:
+            status_code += 0x40000000
+        if self.status["controller_indicator"]:
+            status_code += 0x80000000
+
+        return status_code
+
+    def build_fault_code(self):
+        """"
+            Builds the device fault code
+
+        :return: fault code
+        """
+        fault_code = 0x00000000
+
+        # Laser specific fault bits
+        if self.faults["base_plate_temp_fault"]:
+            fault_code += 0x00000001
+        if self.faults["diode_temp_fault"]:
+            fault_code += 0x00000002
+        if self.faults["internal_temp_fault"]:
+            fault_code += 0x00000004
+        if self.faults["laser_power_supply_fault"]:
+            fault_code += 0x00000008
+        if self.faults["i2c_error"]:
+            fault_code += 0x00000010
+        if self.faults["over_current"]:
+            fault_code += 0x00000020
+        if self.faults["laser_checksum_error"]:
+            fault_code += 0x00000040
+        if self.faults["checksum_recovery"]:
+            fault_code += 0x00000080
+        if self.faults["buffer_overflow"]:
+            fault_code += 0x00000100
+        if self.faults["warm_up_limit_fault"]:
+            fault_code += 0x00000200
+        if self.faults["tec_driver_error"]:
+            fault_code += 0x00000400
+        if self.faults["ccb_error"]:
+            fault_code += 0x00000800
+        if self.faults["diode_temp_limit_error"]:
+            fault_code += 0x00001000
+        if self.faults["laser_ready_fault"]:
+            fault_code += 0x00002000
+        if self.faults["photodiode_fault"]:
+            fault_code += 0x00004000
+        if self.faults["fatal_fault"]:
+            fault_code += 0x00008000
+        if self.faults["startup_fault"]:
+            fault_code += 0x00010000
+        if self.faults["watchdog_timer_reset"]:
+            fault_code += 0x00020000
+        if self.faults["field_calibration"]:
+            fault_code += 0x00040000
+        if self.faults["over_power"]:
+            fault_code += 0x00100000
+
+        # Controller specific fault bits
+        if self.faults["controller_checksum"]:
+            fault_code += 0x40000000
+        if self.faults["controller_status"]:
+            fault_code += 0x80000000
+
+        return fault_code
+
+    @has_log
+    def backdoor_set_status(self, statusname, value):
+        """
+            Sets status code via backdoor
+        :param statusname: name of status attribute
+        :param value: true or false
+        :return: none
+        """
+        try:
+            self.status[statusname] = value
+        except KeyError:
+            self.log.error("An error occurred: " + KeyError.message)
+
+    @has_log
+    def backdoor_set_fault(self, faultname, value):
+        """
+            Sets fault code via backdoor
+        :param faultname: name of fault attribute
+        :param value: true or false
+        :return: none
+        """
+        try:
+            self.faults[faultname] = value
+        except KeyError:
+            self.log.error("An error occurred: " + KeyError.message)
+
     def _get_state_handlers(self):
         return {
             'default': DefaultState(),
@@ -84,16 +225,3 @@ class SimulatedChtobisr(StateMachineDevice):
     def _get_transition_handlers(self):
         return OrderedDict([
         ])
-
-    @has_log
-    def backdoor_set_fault(self, faultname, value):
-        """
-            Sets fault code via backdoor
-        :param faultname: name of attribute
-        :param value: true or false
-        :return: none
-        """
-        try:
-            self.faults[faultname] = value
-        except KeyError:
-            self.log.error("An error occurred: " + str(KeyError))
--- lewis_emulators/chtobisr/interfaces/stream_interface.py
+++ lewis_emulators/chtobisr/interfaces/stream_interface.py
@@ -8,7 +8,7 @@ from lewis_emulators.utils.replies import conditional_reply
 @has_log
 class ChtobisrStreamInterface(StreamInterface):
     """
-    Stream interface for the Coherent OBIS Laser Remote
+        Stream interface for the Coherent OBIS Laser Remote
     """
 
     commands = {
@@ -24,7 +24,7 @@ class ChtobisrStreamInterface(StreamInterface):
 
     def handle_error(self, request, error):
         """
-        If command is not recognised, print and error
+            If command is not recognised, print and error
 
         Args:
             request: requested string
@@ -36,7 +36,7 @@ class ChtobisrStreamInterface(StreamInterface):
     @conditional_reply("connected")
     def get_id(self):
         """
-        Gets the device Identification string
+            Gets the device Identification string
 
         :return:  Device ID string
         """
@@ -46,7 +46,7 @@ class ChtobisrStreamInterface(StreamInterface):
     @conditional_reply("connected")
     def set_reset(self):
         """
-        Resets the device
+            Resets the device
 
         :return:  none
         """
@@ -56,7 +56,7 @@ class ChtobisrStreamInterface(StreamInterface):
     @conditional_reply("connected")
     def get_interlock(self):
         """
-        Gets the device interlock status
+            Gets the device interlock status
 
         :return: Interlock status
         """
@@ -64,147 +64,12 @@ class ChtobisrStreamInterface(StreamInterface):
         return "{}".format(self._device.interlock)
 
     @conditional_reply("connected")
-    def build_status_code(self):
-        """"
-        Builds the device status code
-
-        :return: status code
-        """
-        status_code = 0x00000000
-
-        # Laser specific status bits
-
-        if self.status.laser_fault:
-            status_code += 0x00000001
-
-        if self.status.laser_emission:
-            status_code += 0x00000002
-
-        if self.status.laser_ready:
-            status_code += 0x00000004
-
-        if self.status.laser_standby:
-            status_code += 0x00000008
-
-        if self.status.cdrh_delay:
-            status_code += 0x00000010
-
-        if self.status.laser_hardware_fault:
-            status_code += 0x00000020
-
-        if self.status.laser_error:
-            status_code += 0x00000040
-
-        if self.status.laser_power_calibration:
-            status_code += 0x00000080
-
-        if self.status.laser_warm_up:
-            status_code += 0x00000100
-
-        if self.status.laser_noise:
-            status_code += 0x00000200
-
-        if self.status.external_operating_mode:
-            status_code += 0x00000400
-
-        if self.status.field_calibration:
-            status_code += 0x00000800
-
-        if self.status.laser_power_voltage:
-            status_code += 0x00001000
-
-        # Controller specific status bits
-
-        if self.status.controller_standby:
-            status_code += 0x02000000
-
-        if self.status.controller_interlock:
-            status_code += 0x04000000
-
-        if self.status.controller_enumeration:
-            status_code += 0x08000000
-
-        if self.status.controller_error:
-            status_code += 0x10000000
-
-        if self.status.controller_fault:
-            status_code += 0x20000000
-
-        if self.status.remote_active:
-            status_code += 0x40000000
-
-        if self.status.controller_indicator:
-            status_code += 0x80000000
-
-        return status_code
-
-    @conditional_reply("connected")
     def get_status(self):
         """
             Returns status code
-        :return: status code
+        :return: Formatted status code
         """
-
-        return self.build_status_code()
-
-    @conditional_reply("connected")
-    def build_fault_code(self):
-        """"
-            Builds the device fault code
-
-        :return: fault code
-        """
-        fault_code = 0x00000000
-
-        # Laser specific fault bits
-        if self.faults["base_plate_temp_fault"]:
-            fault_code += 0x00000001
-        if self.faults["diode_temp_fault"]:
-            fault_code += 0x00000002
-        if self.faults["internal_temp_fault"]:
-            fault_code += 0x00000004
-        if self.faults["laser_power_supply_fault"]:
-            fault_code += 0x00000008
-        if self.faults["i2c_error"]:
-            fault_code += 0x00000010
-        if self.faults["over_current"]:
-            fault_code += 0x00000020
-        if self.faults["laser_checksum_error"]:
-            fault_code += 0x00000040
-        if self.faults["checksum_recovery"]:
-            fault_code += 0x00000080
-        if self.faults["buffer_overflow"]:
-            fault_code += 0x00000100
-        if self.faults["warm_up_limit_fault"]:
-            fault_code += 0x00000200
-        if self.faults["tec_driver_error"]:
-            fault_code += 0x00000400
-        if self.faults["ccb_error"]:
-            fault_code += 0x00000800
-        if self.faults["diode_temp_limit_error"]:
-            fault_code += 0x00001000
-        if self.faults["laser_ready_fault"]:
-            fault_code += 0x00002000
-        if self.faults["photodiode_fault"]:
-            fault_code += 0x00004000
-        if self.faults["fatal_fault"]:
-            fault_code += 0x00008000
-        if self.faults["startup_fault"]:
-            fault_code += 0x00010000
-        if self.faults["watchdog_timer_reset"]:
-            fault_code += 0x00020000
-        if self.faults["field_calibration"]:
-            fault_code += 0x00040000
-        if self.faults["over_power"]:
-            fault_code += 0x00100000
-
-        # Controller specific fault bits
-        if self.faults["controller_checksum"]:
-            fault_code += 0x40000000
-        if self.faults["controller_status"]:
-            fault_code += 0x80000000
-
-        return fault_code
+        return "{:08X}".format(self._device.build_status_code())
 
     @conditional_reply("connected")
     def get_faults(self):
@@ -212,4 +77,4 @@ class ChtobisrStreamInterface(StreamInterface):
             Returns faults code
         :return: Formatted fault code
         """
-        return "{%8D}".format(self.build_fault_code())
+        return "{:08X}".format(self._device.build_fault_code())
