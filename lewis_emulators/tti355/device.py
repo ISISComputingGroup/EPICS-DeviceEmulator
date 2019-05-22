2d8b96fa63dda6ca3930b46edbefc4d49310b08a
Kirkham <sophie.kirkham@stfc.ac.uk>
2019-05-22 16:00:16 +0100

    Started building emulator

--- lewis_emulators/tti355/device.py
+++ lewis_emulators/tti355/device.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from states import DefaultState
 from lewis.devices import StateMachineDevice
+from random import random
 
 
 class SimulatedTti355(StateMachineDevice):
@@ -9,7 +10,22 @@ class SimulatedTti355(StateMachineDevice):
         """
         Initialize all of the device's attributes.
         """
-        pass
+        self.ident = "THURLBY EX355P, <version>"
+        self.voltage = 1.00
+        self.voltage_sp = 1.00
+        self.current = 1.00
+        self.current_sp = 1.00
+        self.output_status = "OUT OFF"
+        self.output_status_sp = "OUT OFF"
+        self.output_mode = "M CC"
+        self.output_mode_sp = "M CC"
+        self.error = 0
+
+        self._max_voltage = 35.0
+        self._max_current = 5.0
+
+    def reset(self):
+        self._initialize_data()
 
     def _get_state_handlers(self):
         return {
@@ -23,3 +39,41 @@ class SimulatedTti355(StateMachineDevice):
         return OrderedDict([
         ])
 
+    def get_voltage(self):
+        if self.output_status == "OUT ON":
+            self.voltage = self.voltage_sp + random()
+        else:
+            self.voltage = random()
+        return self.voltage()
+    
+    def set_voltage_sp(self, voltage):
+        voltage = round(float(voltage), 2)
+        if voltage > self._max_voltage:
+            self.error = 2
+        else:
+            self.voltage_sp = voltage
+
+    
+    def get_current(self):
+        pass
+    
+    def set_current_sp(self):
+        pass
+    
+    def set_output_status(self):
+        pass
+
+    def get_output_status(self):
+        pass
+
+    def set_output_mode(self):
+        pass
+
+    def get_output_mode(self):
+        pass        
+    
+    def get_ident(self):
+        pass
+    
+    def get_error(self):
+        pass
--- lewis_emulators/tti355/interfaces/stream_interface.py
+++ lewis_emulators/tti355/interfaces/stream_interface.py
@@ -1,12 +1,39 @@
-from lewis.adapters.stream import StreamInterface, Cmd
+from lewis.adapters.stream import StreamInterface
+from lewis_emulators.utils.command_builder import CmdBuilder
 
 
 class Tti355StreamInterface(StreamInterface):
 
-    # Commands that we expect via serial during normal operation
-    commands = {
-        Cmd("catch_all", "^#9.*$"),  # Catch-all command for debugging
-    }
+    in_terminator = "\n"
+    out_terminator = "\r\n"
+       
+    def __init__(self):
+
+        super(Tti355StreamInterface, self).__init__()
+        # Commands that we expect via serial during normal operation
+        self.commands = {
+            #CmdBuilder(self.get).escape("*IDN?").eos().build(),
+            CmdBuilder(self.get_voltage_sp).escape("V?").eos().build(),
+            CmdBuilder(self.set_voltage_sp).escape("V ").float().eos().build(),
+            CmdBuilder(self.get_voltage).escape("VO?").eos().build(),
+        }
 
     def catch_all(self):
         pass
+    
+    def handle_error(self, request, error):
+        err_string = "command was: {}, error was: {}: {}\n".format(request, error.__class__.__name__, error)
+        print(err_string)
+        self.log.error(err_string)
+        return err_string
+
+    def get_voltage(self):
+        volt = self.device.get_voltage()
+        return "V{:.2f}".format(volt)
+    
+    def get_voltage_sp(self):
+        return "V{:.2f}".format(self.device.voltage_sp)
+
+    def set_voltage_sp(self, voltage_sp):
+        self.device.set_voltage_sp(float(voltage_sp))
+        return self.out_terminator
