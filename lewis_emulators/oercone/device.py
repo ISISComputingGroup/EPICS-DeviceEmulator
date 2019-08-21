2b91c77eeeea3aafc09fb6de037f1f3f81f802c5
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2019-08-21 14:02:34 +0100

    Pressure tests and measurement units test now working

--- lewis_emulators/oercone/device.py
+++ lewis_emulators/oercone/device.py
@@ -1,6 +1,15 @@
 from collections import OrderedDict
 from states import DefaultState
 from lewis.devices import StateMachineDevice
+from enum import Enum, unique
+
+
+@unique
+class Units(Enum):
+    mbar = 0
+    Torr = 1
+    Pascal = 2
+    Micron = 3
 
 
 class SimulatedOercone(StateMachineDevice):
@@ -9,7 +18,8 @@ class SimulatedOercone(StateMachineDevice):
         """
         Initialize all of the device's attributes.
         """
-        pass
+        self.pressure = 0
+        self._measurement_unit = Units.mbar
 
     def _get_state_handlers(self):
         return {
@@ -22,4 +32,25 @@ class SimulatedOercone(StateMachineDevice):
     def _get_transition_handlers(self):
         return OrderedDict([
         ])
+
+    @property
+    def measurement_unit(self):
+        return self._measurement_unit
+
+    @measurement_unit.setter
+    def measurement_unit(self, units):
+        self._measurement_unit = units
+
+    def backdoor_set_unit(self, unit):
+        """
+        Sets unit on device. Called only via the backdoor using lewis.
+
+        Args:
+            unit: integer 0, 1, 2, or 3
+
+        Returns:
+            None
+        """
+        self.measurement_unit = Units(unit)
+
 
--- lewis_emulators/oercone/interfaces/stream_interface.py
+++ lewis_emulators/oercone/interfaces/stream_interface.py
@@ -1,12 +1,40 @@
 from lewis.adapters.stream import StreamInterface, Cmd
+from lewis_emulators.utils.command_builder import CmdBuilder
+from lewis.core.logging import has_log
+from time import sleep
 
 
+@has_log
 class OerconeStreamInterface(StreamInterface):
 
     # Commands that we expect via serial during normal operation
     commands = {
-        Cmd("catch_all", "^#9.*$"),  # Catch-all command for debugging
+        CmdBuilder("enquiry").escape("05").eos().build(),
+        CmdBuilder("get_pressure").escape("PR1\r").eos().build(),
+        CmdBuilder("get_measurement_unit").escape("UNI\r").eos().build()
     }
 
-    def catch_all(self):
+    in_terminator = "\n"
+    out_terminator = "\n"
+
+    def handle_error(self, request, error):
+        err_string = "command was: {}, error was: {}: {}\n".format(request, error.__class__.__name__, error)
+        print(err_string)
+        self.log.error(err_string)
+        return err_string
+
+    def setup_channel(self):
         pass
+
+    def enquiry(self):
+        pass
+
+    def get_pressure(self):
+        # The protocol for talking to the device has to wait 100ms
+        sleep(0.1)
+        return self._device.pressure
+
+    def get_measurement_unit(self):
+        # Wait 100 ms as that is what is in the protocol from the ticket
+        sleep(0.1)
+        return self._device.measurement_unit.value
