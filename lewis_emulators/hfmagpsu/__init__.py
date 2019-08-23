5a77325463878aeb5e9ae9923ff999a4fda8836f
ChrisColeExpControl <christopher.cole@stfc.ac.uk>
2019-08-23 15:55:25 +0100

    Add template emulator

---
+++ lewis_emulators/hfmagpsu/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedHfmagpsu
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedHfmagpsu']
---
+++ lewis_emulators/hfmagpsu/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import HfmagpsuStreamInterface
+
+__all__ = ['HfmagpsuStreamInterface']
