66a853d5bce25f4194f146fb8cd7dc2ea89507e7
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2019-08-29 16:00:53 +0100

    Reworked emulator to more accurately reflect the protocol of the device, use an ENQ character and not sleep

--- lewis_emulators/oercone/device.py
+++ lewis_emulators/oercone/device.py
@@ -12,14 +12,21 @@ class Units(Enum):
     Micron = 3
 
 
+@unique
+class ReadState(Enum):
+    PR1 = 0
+    UNI = 1
+
+
 class SimulatedOercone(StateMachineDevice):
 
     def _initialize_data(self):
         """
         Initialize all of the device's attributes.
         """
-        self.pressure = 0
+        self._pressure = 0
         self._measurement_unit = Units.mbar
+        self._read_state = None
 
     def _get_state_handlers(self):
         return {
@@ -34,11 +41,49 @@ class SimulatedOercone(StateMachineDevice):
         ])
 
     @property
+    def pressure(self):
+        """
+        Returns the value of the pressure sensor.
+
+        Returns:
+            float: Pressure value.
+        """
+        return self._pressure
+
+    @pressure.setter
+    def pressure(self, pressure):
+        """
+        Sets the pressure sensor.
+
+        Args:
+            pressure: Value to set pressure sensor to.
+
+        Returns:
+            None
+        """
+        self._pressure = pressure
+
+    @property
     def measurement_unit(self):
+        """
+        Returns the value of the pressure sensor.
+
+        Returns:
+            Units: The enum unit currently in use e.g. Units.Micron.
+        """
         return self._measurement_unit
 
     @measurement_unit.setter
     def measurement_unit(self, units):
+        """
+        Sets the curent units.
+
+        Args:
+            units (Units member): Enum value to set the units to.
+
+        Returns:
+            None
+        """
         self._measurement_unit = units
 
     def backdoor_set_units(self, unit):
@@ -52,5 +97,28 @@ class SimulatedOercone(StateMachineDevice):
             None
         """
         self.measurement_unit = Units(int(unit))
+
+    @property
+    def read_state(self):
+        """
+        Returns the readstate for the device
+
+        Returns:
+            Enum: Readstate of the device.
+        """
+        return self._read_state
+
+    @read_state.setter
+    def read_state(self, state):
+        """
+        Sets the readstate of the device
+
+        Args:
+            state: Enum readstate of the device to be set
+
+        Returns:
+            None
+        """
+        self._read_state = state
 
 
--- lewis_emulators/oercone/interfaces/stream_interface.py
+++ lewis_emulators/oercone/interfaces/stream_interface.py
@@ -2,7 +2,8 @@ from lewis.adapters.stream import StreamInterface, Cmd
 from lewis_emulators.utils.command_builder import CmdBuilder
 from lewis.core.logging import has_log
 from time import sleep
-from ..device import Units
+from ..device import Units, ReadState
+from lewis_emulators.utils.constants import ACK, ENQ
 
 
 @has_log
@@ -10,39 +11,114 @@ class OerconeStreamInterface(StreamInterface):
 
     # Commands that we expect via serial during normal operation
     commands = {
-        CmdBuilder("enquiry").escape("05").eos().build(),
-        CmdBuilder("get_pressure").escape("PR1\r").eos().build(),
-        CmdBuilder("get_measurement_unit").escape("UNI\r").eos().build(),
-        CmdBuilder("set_measurement_unit").escape("UNI,").arg("0|1|2|3", argument_mapping=int).escape("\r").eos().build()
+        CmdBuilder("handle_enquiry").escape(ENQ).build(),
+        CmdBuilder("acknowledge_pressure").escape("PR1\r").eos().build(),
+        CmdBuilder("acknowledge_measurement_unit").escape("UNI\r").eos().build(),
+        CmdBuilder("acknowledge_set_measurement_unit").escape("UNI,").arg("0|1|2|3", argument_mapping=int).
+            escape("\r").eos().build()
     }
 
     in_terminator = "\n"
     out_terminator = "\n"
 
     def handle_error(self, request, error):
+        """
+        Prints and logs an error message if a command is not recognised.
+
+        Args:
+            request : Request.
+            error: The error that has occurred.
+        Returns:
+            String: The error string.
+        """
         err_string = "command was: \"{}\", error was: {}: {}\n".format(request, error.__class__.__name__, error)
         print(err_string)
         self.log.error(err_string)
         return err_string
 
-    def setup_channel(self):
-        pass
+    def handle_enquiry(self):
+        """
+        Handles an enquiry using the last command sent.
 
-    def enquiry(self):
-        pass
+        Returns:
+            String: Channel pressure if last command was in channels.
+            String: Returns the devices current units if last command is 'UNI'.
+            None: Sets the devices units to 1,2, or 3 if last command is 'UNI{}' where {} is 1, 2 or 3
+                respectively.
+            None: Last command unknown.
+        """
+        units_flags = range(1, 5)
+
+        self.log.info("Mode: {}".format(self._device._read_state.name))
+
+        if self._device._read_state.name == "PR1":
+            return self.get_pressure()
+        elif self._device._read_state.name == "UNI":
+            return self.get_measurement_unit()
+        else:
+            self.log.info("Last command was unknown. Current readstate is {}.".format(self._device._read_state))
+            print("Last command was unknown. Current readstate is {}.".format(self._device._read_state))
+
+    def acknowledge_pressure(self):
+        """
+        Acknowledges a request to get the pressure and stores the request.
+
+        Returns:
+            ASCII acknowledgement character (0x6).
+        """
+        self._device._read_state = ReadState["PR1"]
+        return ACK
+
+    def acknowledge_measurement_unit(self):
+        """
+        Acknowledge that the request to get the units was received.
+
+        Returns:
+            ASCII acknowledgement character (0x6).
+        """
+        self._device._read_state = ReadState["UNI"]
+        return ACK
+
+    def acknowledge_set_measurement_unit(self, units):
+        """
+        Acknowledge that the request to set the units was received.
+
+        Args:
+            units (integer): Takes the value 1, 2 or 3.
+
+        Returns:
+            ASCII acknowledgement character (0x6).
+        """
+        self._device._read_state = ReadState["UNI"]
+        self._device.measurement_unit = Units(units)
+        return ACK
 
     def get_pressure(self):
-        # The protocol for talking to the device has to wait 100ms
-        sleep(0.1)
-        return int(self._device.pressure)
+        """
+        Gets the pressure for the device.
+
+        Returns:
+            String: Pressure from the channel.
+        """
+        return "0,{}\r\n".format(self._device.pressure)
 
     def get_measurement_unit(self):
-        # The protocol for talking to the device has to wait 100ms
-        sleep(0.1)
-        return self._device.measurement_unit.value
+        """
+        Gets the units of the device.
+
+        Returns:
+            Name of the units.
+        """
+        return "{}\r\n".format(self._device.measurement_unit.value)
 
     def set_measurement_unit(self, units):
-        # The protocol for talking to the device has to wait 100ms
-        sleep(0.1)
-        self.log.info("Setting measurement unit to {}".format(units))
+        """
+       Sets the units on the device.
+
+       Args:
+           units (Units member): Units to be set
+
+       Returns:
+           None
+       """
         self._device.measurement_unit = Units(units)
