92b05440304c3baa543f4e4b3d0ab3e1e63b864b
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-22 16:23:55 +0100

    added ice fridge emulator that is returning temp setpoint

---
+++ lewis_emulators/icefrdge/interfaces/stream_interface.py
@@ -0,0 +1,38 @@
+from lewis.adapters.stream import StreamInterface, Cmd
+from lewis_emulators.utils.command_builder import CmdBuilder
+from lewis.core.logging import has_log
+from lewis_emulators.utils.constants import ACK, ENQ
+
+
+@has_log
+class IceFridgeStreamInterface(StreamInterface):
+
+    # Commands that we expect via serial during normal operation
+    commands = {
+        CmdBuilder("set_temp_setpoint").escape("AUTO TSET=").float().eos().build(),
+        CmdBuilder("get_temp_set_RBV").escape("AUTO TSET?").eos().build()
+    }
+
+    in_terminator = "\n"
+    out_terminator = "\n"
+
+    def handle_error(self, request, error):
+        """
+        Prints and logs an error message if a command is not recognised.
+
+        Args:
+            request : Request.
+            error: The error that has occurred.
+        Returns:
+            String: The error string.
+        """
+        err_string = "command was: \"{}\", error was: {}: {}\n".format(request, error.__class__.__name__, error)
+        print(err_string)
+        self.log.error(err_string)
+        return err_string
+
+    def set_temp_setpoint(self, temp_setpoint):
+        self._device.temp_setpoint = temp_setpoint
+
+    def get_temp_set_RBV(self):
+        return self._device.temp_setpoint
