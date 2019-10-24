344a96b2f9c43af8b3463d14501a2da4bc1ce8bf
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-24 14:14:24 +0100

    removed useless properties, using fields directly

--- lewis_emulators/icefrdge/device.py
+++ lewis_emulators/icefrdge/device.py
@@ -9,28 +9,12 @@ class SimulatedIceFridge(StateMachineDevice):
         """
         Initialize all of the device's attributes.
         """
-        self._auto_temp_setpoint = 0
-        self._manual_temp_setpoint = 0
+        self.auto_temp_setpoint = 0
+        self.manual_temp_setpoint = 0
         self.vti_temp1 = 0
         self.vti_temp2 = 0
         self.vti_temp3 = 0
         self.vti_temp4 = 0
-
-    @property
-    def auto_temp_setpoint(self):
-        return self._auto_temp_setpoint
-
-    @auto_temp_setpoint.setter
-    def auto_temp_setpoint(self, new_temp_setpoint):
-        self._auto_temp_setpoint = new_temp_setpoint
-
-    @property
-    def manual_temp_setpoint(self):
-        return self._manual_temp_setpoint
-
-    @manual_temp_setpoint.setter
-    def manual_temp_setpoint(self, new_temp_setpoint):
-        self._manual_temp_setpoint = new_temp_setpoint
 
     def _get_state_handlers(self):
         return {
