56256248e8eacc6a09edc9116d1c5dfdebd58c0b
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-04-28 16:47:21 +0100

    Initial emulator

---
+++ lewis_emulators/mecfrf/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedMecfrf
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedMecfrf']
---
+++ lewis_emulators/mecfrf/interfaces/stream_interface.py
@@ -0,0 +1,75 @@
+import six
+from lewis.adapters.stream import StreamInterface, Cmd
+import threading
+import struct
+
+
+EXPECTED_MESSAGE_LENGTH = 188
+
+
+class MecfrfStreamInterface(StreamInterface):
+
+    # Commands that we expect via serial during normal operation (No commands - the device always sends a stream of
+    # information to the IOC without being polled)
+    commands = {}
+
+    in_terminator = ""
+    out_terminator = ""
+
+    def __init__(self):
+        super(MecfrfStreamInterface, self).__init__()
+        self._queue_next_unsolicited_message()
+
+    def _queue_next_unsolicited_message(self):
+        timer = threading.Timer(1.0, self.get_data_unsolicited)
+        timer.daemon = True
+        timer.start()
+
+    def handle_error(self, request, error):
+        print("An error occurred at request " + repr(request) + ": " + repr(error))
+        return str(error)
+
+    def get_data_unsolicited(self):
+        self._queue_next_unsolicited_message()
+
+        if not self.device.connected:
+            return
+
+        try:
+            handler = self.handler
+        except AttributeError:
+            # Happens if no client is currently connected.
+            return
+        else:
+            if self.device.corrupted_messages:
+                # Nonsense message which should cause alarms in the IOC.
+                handler.unsolicitedReply(b"A" * EXPECTED_MESSAGE_LENGTH)
+            else:
+                handler.unsolicitedReply(self._construct_status_message())
+
+    def _construct_status_message(self):
+        # Fixed message "preamble"
+        msg = six.binary_type("DATA")
+
+        # There are 6 integer header fields which we ignore. They are:
+        # - Order number
+        # - Serial number
+        # - Length of measurement data
+        # - Length of video data
+        # - Frame number
+        # - Counter
+        for _ in range(6):
+            msg += struct.pack("L", 0)
+
+        # Two little-endian signed integers corresponding to the data for sensors 1 and 2 respectively.
+        msg += struct.pack("<l", int(self.device.sensor1))
+        msg += struct.pack("<l", int(self.device.sensor2))
+
+        # 38 more integers corresponding to video data (???), which we ignore.
+        for _ in range(38):
+            msg += struct.pack("L", 0)
+
+        assert len(msg) == EXPECTED_MESSAGE_LENGTH, \
+            "Message length {} was expected to be {}".format(len(msg), EXPECTED_MESSAGE_LENGTH)
+
+        return msg
