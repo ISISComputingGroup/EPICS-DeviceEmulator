14aac546348b8eb2c5e3a29f6ae2c8cbba3a9a81
Alexandru Dascalu <965337@swansea.ac.uk>
2020-05-07 09:10:05 +0100

    corrected some logic

--- lewis_emulators/fins/interfaces/stream_interface.py
+++ lewis_emulators/fins/interfaces/stream_interface.py
@@ -52,7 +52,7 @@ class FinsPLCStreamInterface(StreamInterface):
         service_id = ord(command[9])
         self.log.info("Service id: {}".format(service_id))
 
-        if ord(command[10]) != 0x01 and ord(command[11]) != 0x01:
+        if ord(command[10]) != 0x01 or ord(command[11]) != 0x01:
             raise ValueError("The command code should be 0x0101, for memory area read command!")
 
         if ord(command[12]) != 0x82:
@@ -113,30 +113,3 @@ class FinsPLCStreamInterface(StreamInterface):
 
         for i in range(5, 10):
             check_is_byte(fins_frame_header[i])
-
-    @staticmethod
-    def _check_find_command_frame_validity(fins_command_frame):
-
-        if ord(fins_command_frame[10]) != 0x01 and ord(fins_command_frame[11]) != 0x01:
-            raise ValueError("The command code should be 0x0101, for memory area read command!")
-
-        if ord(fins_command_frame[12]) != 0x82:
-            raise ValueError("The emulator only supports reading words from the DM area, for which the code is 82.")
-
-        # The FINS PLC supports reading either a certain number of words, or can also read individual bits in a word.
-        # The helium recovery memory map implies that that PLC uses word designated reading. When bit designated
-        # reading is not used, the 16th byte of the command is 0x00.
-        if ord(fins_command_frame[15]) != 0x00:
-            raise ValueError("The emulator only supports word designated memory reading. The bit address must be 0x00")
-
-        number_of_words_to_read = raw_bytes_to_int(fins_command_frame[16:18])
-
-        # The helium recovery PLC memory map has addresses that store types that take up either one word (16 bits) or
-        # two. Most take up one word, so if the number of words to read is two we check that the client wants to read
-        # from a memory location from where a 32 bit value starts.
-        if number_of_words_to_read == 2 and memory_start_address not in self.device.double_word_memory_locations:
-            raise ValueError("The memory start address specified corresponds to a single word in the memory map, "
-                             "not two.")
-        elif number_of_words_to_read > 2:
-            raise ValueError("The memory map only specifies data types that should take up two words at most.")
-
