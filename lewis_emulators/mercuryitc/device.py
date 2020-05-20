1b07f24dee4c1b7ce4dae92eb72c3b4e68110505
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-05-20 14:31:35 +0100

    Add basic temp card emulator

---
+++ lewis_emulators/mercuryitc/device.py
@@ -0,0 +1,89 @@
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+from collections import OrderedDict
+from lewis.core.logging import has_log
+from .states import DefaultState
+from lewis.devices import StateMachineDevice
+
+
+class Channel(object):
+    def __init__(self, nickname):
+        super(Channel, self).__init__()
+        self.nickname = nickname
+
+
+class TemperatureChannel(Channel):
+    def __init__(self, nickname):
+        super(TemperatureChannel, self).__init__(nickname)
+        self.channel_type = "TEMP"
+
+        self.temperature = 0
+        self.temperature_sp = 0
+        self.resistance = 0
+        self.calibration_file = "sim_calib_file"
+
+        self.autopid = False
+        self.autopid_file = "sim_autopid_file"
+        self.p = 0
+        self.i = 0
+        self.d = 0
+
+        self.gas_flow_auto = True
+        self.gas_flow = 0
+        self.heater_auto = True
+        self.heater_percent = 0
+
+        self.associated_heater_channel = None
+        self.associated_aux_channel = None
+
+
+class HeaterChannel(Channel):
+    def __init__(self, nickname):
+        super(HeaterChannel, self).__init__(nickname)
+        self.channel_type = "HTR"
+
+        self.power = 0
+        self.voltage = 0
+        self.current = 0
+        self.voltage_limit = 0
+
+
+class AuxChannel(Channel):
+    def __init__(self, nickname):
+        super(AuxChannel, self).__init__(nickname)
+        self.channel_type = "AUX"
+
+
+@has_log
+class SimulatedMercuryitc(StateMachineDevice):
+
+    def _initialize_data(self):
+        self.connected = True
+
+        self.channels = {
+            "MB0": TemperatureChannel("MB0.T0"),
+            "DB0": HeaterChannel("DB0.H0"),
+            "DB1": AuxChannel("DB0.A1"),
+            "MB1": TemperatureChannel("MB1.T1"),
+            "DB2": HeaterChannel("DB0.H1"),
+            "DB3": AuxChannel("DB0.A0"),
+        }
+
+        # Associate each temperature channel with a heater and an auxilary channel:
+        self.channels["MB0"].associated_heater_channel = "DB0"
+        self.channels["MB0"].associated_aux_channel = "DB1"
+
+        self.channels["MB1"].associated_heater_channel = "DB2"
+        self.channels["MB1"].associated_aux_channel = "DB3"
+
+    def reset(self):
+        self._initialize_data()
+
+    def _get_state_handlers(self):
+        return {'default': DefaultState()}
+
+    def _get_initial_state(self):
+        return 'default'
+
+    def _get_transition_handlers(self):
+        return OrderedDict([])
---
+++ lewis_emulators/mercuryitc/interfaces/stream_interface.py
@@ -0,0 +1,110 @@
+from lewis.adapters.stream import StreamInterface
+from lewis.core.logging import has_log
+
+from lewis_emulators.utils.command_builder import CmdBuilder
+from lewis_emulators.utils.replies import conditional_reply
+
+if_connected = conditional_reply("connected")
+
+ISOBUS_PREFIX = "@1"
+
+
+@has_log
+class MercuryitcInterface(StreamInterface):
+    commands = {
+        CmdBuilder("get_catalog").optional(ISOBUS_PREFIX)
+            .escape("READ:SYS:CAT").eos().build(),
+        CmdBuilder("get_nickname").optional(ISOBUS_PREFIX)
+            .escape("READ:DEV:").any().escape(":NICK").eos().build(),
+
+        CmdBuilder("get_all_temp_sensor_details").optional(ISOBUS_PREFIX)
+            .escape("READ:DEV:").any().escape(":TEMP").eos().build()
+    }
+
+    in_terminator = "\n"
+    out_terminator = "\n"
+
+    def handle_error(self, request, error):
+        err_string = "command was: {}, error was: {}: {}\n".format(request, error.__class__.__name__, error)
+        print(err_string)
+        self.log.error(err_string)
+        return err_string
+
+    @if_connected
+    def get_catalog(self):
+        resp = "STAT:SYS:CAT"
+
+        for chan_location, chan in self.device.channels.items():
+            resp += ":DEV:{}:{}".format(chan_location, chan.channel_type)
+
+        return resp
+
+    @if_connected
+    def get_nickname(self, arg):
+
+        deviceid = arg.split(":")[0] if ":" in arg else arg
+
+        if deviceid not in self.device.channels.keys():
+            self.log.error("Can't get nickname for ID {}".format(deviceid))
+            return "STAT:DEV:{}:NICK:INVALID".format(deviceid)
+
+        return "STAT:DEV:{}:NICK:{}".format(deviceid, self.device.channels[deviceid].nickname)
+
+    @if_connected
+    def get_all_temp_sensor_details(self, deviceid):
+        """
+        Gets the details for an entire temperature sensor all at once. This is only used by the LabVIEW VI, not by
+        the IOC (the ioc queries each parameter individually)
+        """
+
+        if deviceid not in self.device.channels.keys():
+            self.log.error("Can't get all details for ID {} (doesn't exist)".format(deviceid))
+            return "STAT:DEV:{}:INVALID".format(deviceid)
+
+        if self.device.channels[deviceid].channel_type != "TEMP":
+            self.log.error("Can't get all details for ID {} (incorrect channel type)".format(deviceid))
+            return "STAT:DEV:{}:INVALID".format(deviceid)
+
+        chan = self.device.channels[deviceid]
+
+        return "STAT:DEV:{}:TEMP:".format(deviceid) + \
+               "EXCT" + \
+                 ":TYPE:UNIP" + \
+                 ":MAG:0" + \
+               ":STAT:40000000" + \
+               ":NICK:{}".format(chan.nickname) + \
+               ":LOOP" + \
+                 ":AUX:{}".format(chan.associated_aux_channel) + \
+                 ":D:{}".format(chan.d) + \
+                 ":HTR:{}".format(chan.associated_heater_channel) + \
+                 ":I:{}".format(chan.i) + \
+                 ":THTF:None" + \
+                 ":HSET:{}".format(chan.heater_percent) + \
+                 ":PIDT:{}".format("ON" if chan.autopid else "OFF") + \
+                 ":ENAB:{}".format("ON" if chan.heater_auto else "OFF") + \
+                 ":SWFL:None" + \
+                 ":FAUT:{}".format("ON" if chan.gas_flow_auto else "OFF") + \
+                 ":FSET:{}".format(chan.gas_flow) + \
+                 ":PIDF:{}".format(chan.autopid_file if chan.autopid else "None") + \
+                 ":P:{}".format(chan.p) + \
+                 ":SWMD:FIX" + \
+                 ":TSET:{:.4f}K".format(chan.temperature_sp) + \
+               ":MAN" + \
+                 ":HVER:1" + \
+                 ":FVER:1.12" + \
+                 ":SERL:111450078" + \
+               ":CAL" + \
+                 ":OFFS:0" + \
+                 ":COLDL:999.00K" + \
+                 ":INT:LIN" + \
+                 ":SCAL:1" + \
+                 ":FILE:{}".format(chan.calibration_file) + \
+                 ":HOTL:999.00K" + \
+                 ":TYPE:TCE" + \
+               ":SIG" + \
+                 ":VOLT:-0.0038mV" + \
+                 ":CURR:-0.0000A" + \
+                 ":TEMP:{:.4f}K".format(chan.temperature) + \
+                 ":POWR:0.0000W" + \
+                 ":RES:{:.4f}O".format(chan.resistance) + \
+                 ":SLOP:0.0000O/K"
