b92c48c0c891c0f9365d9b00333ff040d13956ab
Alexandru Dascalu <965337@swansea.ac.uk>
2020-06-02 16:46:31 +0100

    addes support for new pvs

--- lewis_emulators/fins/device.py
+++ lewis_emulators/fins/device.py
@@ -12,6 +12,17 @@ class SimulatedFinsPLC(StateMachineDevice):
     PV_NAME_TO_MEMORY_MAPPING = {
         "HEARTBEAT": 19500,
         "MCP:BANK1:TS2": 19501,
+        "MCP:BANK1:TS1": 19502,
+        "MCP1:BANK2:IMPURE_HE": 19503,
+        "MCP2:BANK2:IMPURE_HE": 19504,
+        "MCP1:BANK3:MAIN_HE_STORAGE": 19505,
+        "MCP2:BANK3:MAIN_HE_STORAGE": 19506,
+        "MCP1:BANK4:DLS_HE_STORAGE": 19507,
+        "MCP2:BANK4:DLS_HE_STORAGE": 19508,
+        "MCP1:BANK5:SPARE_STORAGE": 19509,
+        "MCP2:BANK5:SPARE_STORAGE": 19510,
+        "MCP1:BANK6:SPARE_STORAGE": 19511,
+        "MCP2:BANK6:SPARE_STORAGE": 19512,
         "HE_PURITY": 19533,
         "DEW_POINT": 19534
     }
@@ -29,6 +40,17 @@ class SimulatedFinsPLC(StateMachineDevice):
         self.memory = {
             19500: 0,  # heartbeat
             19501: 0,  # mcp bank 1 TS2 helium gas resupply
+            19502: 0,  # mcp bank 1 TS1 helium gas resupply
+            19503: 0,  # mcp 1 bank 2 impure helium
+            19504: 0,  # mcp 2 bank 2 impure helium
+            19505: 0,  # mcp 1 bank 3 main helium storage
+            19506: 0,  # mcp 2 bank 3 main helium storage
+            19507: 0,  # mcp 1 bank 4 dls helium storage
+            19508: 0,  # mcp 2 bank 4 dls helium storage
+            19509: 0,  # mcp 1 bank 5 spare storage
+            19510: 0,  # mcp 2 bank 5 spare storage
+            19511: 0,  # mcp 1 bank 6 spare storage
+            19512: 0,  # mcp 2 bank 6 spare storage
             19533: 0,  # helium purity
             19534: 0,  # dew point
         }
