fb42c00dab68f8d988ca642e52dfac5a1b0b8c11
RaiBishal <bishal.rai@ndlt1205.isis.cclrc.ac.uk>
2020-12-07 09:30:26 +0000

    emulator for pt2025

---
+++ lewis_emulators/pt2025/interfaces/stream_interface.py
@@ -0,0 +1,40 @@
+from threading import Timer
+
+from lewis.adapters.stream import StreamInterface, Cmd
+from lewis_emulators.utils.command_builder import CmdBuilder
+from lewis_emulators.utils.replies import conditional_reply
+import threading
+if_connected = conditional_reply("connected")
+DATA = "L12.123456T"
+NUMBER_OF_MESSAGES = 10
+class Pt2025StreamInterface(StreamInterface):
+    commands = {}
+    in_terminator = "\r\n"
+    out_terminator = "\r\n"
+
+    def __init__(self):
+        super(Pt2025StreamInterface, self).__init__()
+        self._queue_next_unsolicited_message()
+
+    def _queue_next_unsolicited_message(self):
+        timer = threading.Timer(1.0, self.get_data_unsolicited)
+        timer.daemon = True
+        timer.start()
+
+    def get_data_unsolicited(self):
+        self._queue_next_unsolicited_message()
+
+        if not self.device.connected:
+            return
+
+        try:
+            handler = self.handler
+        except AttributeError:
+            # Happens if no client is currently connected.
+            return
+        else:
+            handler.unsolicited_reply(str(self.device.data))
+
+
+    def handle_error(self, request, error):
+        pass
