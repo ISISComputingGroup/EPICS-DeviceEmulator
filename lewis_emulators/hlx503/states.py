1771a678b6d1a7c46b22e7baa42f8a023ca6b56c
JamesKingWork <james.c.king@stfc.ac.uk>
2021-03-16 12:49:19 +0000

    Move logic into device; Add states to correctly react to temperature setpoints; Correct properties

--- lewis_emulators/hlx503/states.py
+++ lewis_emulators/hlx503/states.py
@@ -2,12 +2,27 @@ from lewis.core import approaches
 from lewis.core.statemachine import State
 
 
-class DefaultState(State):
-
+class TemperatureControlState(State):
     def in_state(self, dt):
         device = self._context
 
         rate = 10
 
-        for channel in device.magic_box.channels:
-            channel.temperature = approaches.linear(channel.temperature, channel.temperature_sp, rate, dt)
+        device.he3pot_temp = approaches.linear(device.he3pot_temp, device.temperature_sp, rate, dt)
+
+
+class He3PotEmptyState(State):
+
+    def in_state(self, dt):
+        device = self._context
+
+        device.he3pot_temp = approaches.linear(device.he3pot_temp, device.drift_towards, device.drift_rate, dt)
+
+
+class RegeneratingState(State):
+
+    def in_state(self, dt):
+        device = self._context
+
+        device.sorb_temp = approaches.linear(device.sorb_temp, device.temperature_sp, device.heater_voltage, dt)
+        device.he3pot_temp = approaches.linear(device.he3pot_temp, device.drift_towards, device.drift_rate, dt)
