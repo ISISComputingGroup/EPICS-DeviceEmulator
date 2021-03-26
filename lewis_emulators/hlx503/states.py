6d9313e6944ba8ace584dee35b718bbd6f265e0f
JamesKingWork <james.c.king@stfc.ac.uk>
2021-03-26 15:37:17 +0000

    hlx503: Use rate constant rather than heater voltage and allow sorb heating when in He3PotEmptyState

--- lewis_emulators/hlx503/states.py
+++ lewis_emulators/hlx503/states.py
@@ -2,14 +2,15 @@ from lewis.core import approaches
 from lewis.core.statemachine import State
 
 
+RATE = 10
+
+
 class TemperatureControlState(State):
     def in_state(self, dt):
         device = self._context
 
-        rate = 10
-
         device.he3pot_temp = approaches.linear(
-            device.he3pot_temp, device.temperature_sp, rate, dt
+            device.he3pot_temp, device.temperature_sp, RATE, dt
         )
 
 
@@ -18,6 +19,9 @@ class He3PotEmptyState(State):
     def in_state(self, dt):
         device = self._context
 
+        device.sorb_temp = approaches.linear(
+            device.sorb_temp, device.temperature_sp, RATE, dt
+        )
         device.he3pot_temp = approaches.linear(
             device.he3pot_temp, device.he3pot_empty_drift_towards, device.drift_rate, dt
         )
@@ -29,7 +33,7 @@ class RegeneratingState(State):
         device = self._context
 
         device.sorb_temp = approaches.linear(
-            device.sorb_temp, device.temperature_sp, device.heater_voltage, dt
+            device.sorb_temp, device.temperature_sp, RATE, dt
         )
         device.he3pot_temp = approaches.linear(
             device.he3pot_temp, device.he3pot_regenerating_drift_towards, device.drift_rate, dt
