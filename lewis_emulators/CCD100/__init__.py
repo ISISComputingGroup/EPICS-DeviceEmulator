a9b62e79f6a5c84d54ef528d4b0cb3cdc1346056
Dominic Oram <dominic.oram@stfc.ac.uk>
2021-06-10 12:10:20 +0100

    Removed CCD100 as it's now in the support directory

--- lewis_emulators/CCD100/__init__.py
+++
@@ -1,5 +0,0 @@
-from .device import SimulatedCCD100
-from ..lewis_versions import LEWIS_LATEST
-
-framework_version = LEWIS_LATEST
-__all__ = ['SimulatedCCD100']
--- lewis_emulators/CCD100/interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-from .stream_interface import CCD100StreamInterface
-
-__all__ = ['CCD100StreamInterface']
