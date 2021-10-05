1c69cf5616b5b130bf57f5792aae83c77be3ee46
Jack Harper <jack.harper@stfc.ac.uk>
2021-10-05 13:42:14 +0100

    Delete lewis_emulators/ilm200 directory

--- lewis_emulators/ilm200/__init__.py
+++
@@ -1,5 +0,0 @@
-from .device import SimulatedIlm200
-from ..lewis_versions import LEWIS_LATEST
-
-framework_version = LEWIS_LATEST
-__all__ = ['SimulatedIlm200']
--- lewis_emulators/ilm200/interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-from .stream_interface import Ilm200StreamInterface
-
-__all__ = ['Ilm200StreamInterface']
