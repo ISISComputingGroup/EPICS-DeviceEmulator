47a94fe30744d2466e24d5d066bf1c4f9baf1af7
aaronlong <aaron.long@stfc.ac.uk>
2021-10-28 16:23:57 +0100

    removed old tests

--- lewis_emulators/dh2000/__init__.py
+++
@@ -1,5 +0,0 @@
-from .device import SimulatedDh2000
-from ..lewis_versions import LEWIS_LATEST
-
-framework_version = LEWIS_LATEST
-__all__ = ['SimulatedDh2000']
--- lewis_emulators/dh2000/interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-from .stream_interface import Dh2000StreamInterface
-
-__all__ = ['Dh2000StreamInterface']
