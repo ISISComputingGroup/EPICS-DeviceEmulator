ee3e98eff2bcd10bb6a5f24e8efcd925f360dce4
aaronlong <aaron.long@stfc.ac.uk>
2021-11-02 15:43:39 +0000

    removed old emulator

--- lewis_emulators/ag33220a/__init__.py
+++
@@ -1,5 +0,0 @@
-from .device import SimulatedAG33220A
-from ..lewis_versions import LEWIS_LATEST
-
-framework_version = LEWIS_LATEST
-__all__ = ['SimulatedAG33220A']
--- lewis_emulators/ag33220a/interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-from .stream_interface import AG33220AStreamInterface
-
-__all__ = ['AG33220AStreamInterface']
