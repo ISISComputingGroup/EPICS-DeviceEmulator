6f6899d65a5804ea40e3762fed04c128613cda3e
HannahCawley <HannahCawley@NDW1798.isis.cclrc.ac.uk>
2022-08-24 16:25:24 +0100

    Add emulator property 'connected' to fix refactoring

--- lewis_emulators/danfysik/interfaces/dfkps_base.py
+++ lewis_emulators/danfysik/interfaces/dfkps_base.py
@@ -3,10 +3,11 @@ Stream device for danfysik
 """
 import abc
 import six
-
+import functools
 from lewis.core.logging import has_log
 from lewis.utils.command_builder import CmdBuilder
 from lewis.utils.replies import conditional_reply
+
 
 
 @has_log
@@ -18,6 +19,7 @@ class CommonStreamInterface(object):
 
     in_terminator = "\r"
     out_terminator = ""
+
 
     commands = [
         CmdBuilder("get_voltage").escape("AD 2").eos().build(),
@@ -40,45 +42,56 @@ class CommonStreamInterface(object):
         """
         self.log.error("An error occurred at request " + repr(request) + ": " + repr(error))
 
+
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def get_current(self):
         return int(round(self.device.get_current()))
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def set_current(self, value):
         self.device.set_current(value)
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def get_last_setpoint(self):
         return int(round(self.device.get_last_setpoint()))
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def get_voltage(self):
         return int(round(self.device.get_voltage()))
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def unlock(self):
         """
         Unlock the device. Implementation could be put in in future.
         """
         
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def get_polarity(self):
         return "-" if self.device.negative_polarity else "+"
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def set_polarity(self, polarity):
         assert polarity in ["+", "-"]
         self.device.negative_polarity = polarity == "-"
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def set_power_off(self):
         self.device.power = False
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def set_power_on(self):
         self.device.power = True
 
+    @conditional_reply("connected")
     @conditional_reply("comms_initialized")
     def reset_device(self):
         self.device.reset()
@@ -89,6 +102,7 @@ class CommonStreamInterface(object):
         Respond to the get_status command.
         """
 
+    @conditional_reply("connected")
     def init_comms(self):
         """
         Initialize comms of device
