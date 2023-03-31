c008c9cefb04d4d24842b323d1a0920b4b7e4add
Jack Harper <jack.harper@stfc.ac.uk>
2023-03-31 16:18:21 +0100

    add skeleton modbus skf chopper emulator

---
+++ lewis_emulators/skf_chopper/device.py
@@ -0,0 +1,65 @@
+from collections import OrderedDict
+
+from lewis.devices import StateMachineDevice
+from .states import DefaultState
+
+
+class SimulatedSKFChopper(StateMachineDevice):
+    """
+    Simulated Eurotherm temperature sensor.
+    """
+
+    def _initialize_data(self):
+        """
+        Sets the initial state of the device.
+        """
+        self.connected = True
+
+        self.state = 0
+        self.shaft_angle = 0
+        self._address = "A1"
+        self.speed = 0
+        self.freq = 40 
+        self.phasens = 4000
+        self._send_correct_transaction_id = True
+
+
+    def _get_state_handlers(self):
+        """
+        Returns: states and their names
+        """
+        return {
+            DefaultState.NAME: DefaultState()
+        }
+
+    def _get_initial_state(self):
+        """
+        Returns: the name of the initial state
+        """
+        return DefaultState.NAME
+
+    def _get_transition_handlers(self):
+        """
+        Returns: the state transitions
+        """
+        return OrderedDict()
+
+    @property
+    def address(self):
+        """
+        Get the address of the device.
+
+        Returns: the address of the device e.g. "A01"
+        """
+        return self._address
+
+    @address.setter
+    def address(self, addr):
+        """
+        Sets the address of the device.
+
+        Args:
+            addr (str): the address of this device e.g. "A01".
+
+        """
+        self._address = addr
