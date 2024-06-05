48e4f4e5514204af0da4855b29380fa1228c7e12
esmithExperimentControls <evan.smith@stfc.ac.uk>
2024-06-05 15:48:02 +0100

    Change regex to escape to ensure strings aren't passed or modified

--- lewis_emulators/eurotherm/interfaces/stream_interface.py
+++ lewis_emulators/eurotherm/interfaces/stream_interface.py
@@ -11,20 +11,20 @@ class EurothermStreamInterface(StreamInterface):
     """
 
     commands = {
-        CmdBuilder("get_current_temperature").eot().int().regex("PV").enq().build(),
-        CmdBuilder("get_setpoint").eot().int().regex("SL").enq().build(),
-        CmdBuilder("get_ramp_setpoint").eot().int().regex("SP").enq().build(),
-        CmdBuilder("get_output").eot().int().regex("OP").enq().build(),
-        CmdBuilder("get_max_output").eot().int().regex("HO").enq().build(),
-        CmdBuilder("get_output_rate").eot().int().regex("OR").enq().build(),
-        CmdBuilder("get_autotune").eot().int().regex("AT").enq().build(),
-        CmdBuilder("get_proportional").eot().int().regex("XP").enq().build(),
-        CmdBuilder("get_derivative").eot().int().regex("TD").enq().build(),
-        CmdBuilder("get_integral").eot().int().regex("TI").enq().build(),
-        CmdBuilder("get_highlim").eot().int().regex("HS").enq().build(),
-        CmdBuilder("get_lowlim").eot().int().regex("LS").enq().build(),
-        CmdBuilder("get_error").eot().int().regex("EE").enq().build(),
-        CmdBuilder("get_address").eot().int().regex("").enq().build(),
+        CmdBuilder("get_current_temperature").eot().int().escape("PV").enq().build(),
+        CmdBuilder("get_setpoint").eot().int().escape("SL").enq().build(),
+        CmdBuilder("get_ramp_setpoint").eot().int().escape("SP").enq().build(),
+        CmdBuilder("get_output").eot().int().escape("OP").enq().build(),
+        CmdBuilder("get_max_output").eot().int().escape("HO").enq().build(),
+        CmdBuilder("get_output_rate").eot().int().escape("OR").enq().build(),
+        CmdBuilder("get_autotune").eot().int().escape("AT").enq().build(),
+        CmdBuilder("get_proportional").eot().int().escape("XP").enq().build(),
+        CmdBuilder("get_derivative").eot().int().escape("TD").enq().build(),
+        CmdBuilder("get_integral").eot().int().escape("TI").enq().build(),
+        CmdBuilder("get_highlim").eot().int().escape("HS").enq().build(),
+        CmdBuilder("get_lowlim").eot().int().escape("LS").enq().build(),
+        CmdBuilder("get_error").eot().int().escape("EE").enq().build(),
+        CmdBuilder("get_address").eot().int().escape("").enq().build(),
 
         CmdBuilder("set_ramp_setpoint", arg_sep="").eot().int().stx().escape("SL").float().etx().any().build(),
         CmdBuilder("set_output_rate", arg_sep="").eot().int().stx().escape("OR").float().etx().any().build(),
@@ -59,11 +59,11 @@ class EurothermStreamInterface(StreamInterface):
         self.log.error("An error occurred at request " + repr(request) + ": " + repr(error))
 
     @if_connected
-    def get_address(self):
+    def get_address(self, addr):
         """
         Get the address of the specific Eurotherm sensor, i.e. A01 or 0011
         """
-        return self.make_read_reply(self.device.address())
+        return self.make_read_reply(self.device.address(addr))
     
     @if_connected
     def get_setpoint(self, addr):
