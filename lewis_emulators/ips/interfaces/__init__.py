e7250b38876dfe372be1016dea7095ec3dcea5e7
Ian Gillingham <ian.gillingham@stfc.ac.uk>
2025-03-20 09:33:45 +0000

    __init__.py: Added additional stream protocol module for SCPI

--- lewis_emulators/ips/interfaces/__init__.py
+++ lewis_emulators/ips/interfaces/__init__.py
@@ -1,3 +1,4 @@
 from .stream_interface import IpsStreamInterface
+from .stream_interface_scpi import IpsSCPIStreamInterface
 
-__all__ = ["IpsStreamInterface"]
+__all__ = ["IpsStreamInterface", "IpsSCPIStreamInterface"]
