c854d1a45259d22b68cebec0b2a3990a295b5907
Ian Gillingham <ian.gillingham@stfc.ac.uk>
2025-07-10 09:42:56 +0100

    Deleted the ips directory as this has now moved to EPICS\support\ips

--- lewis_emulators/ips/__init__.py
+++
@@ -1,6 +0,0 @@
-from ..lewis_versions import LEWIS_LATEST
-# from .device import SimulatedIps
-from .device_scpi import SimulatedIps
-
-framework_version = LEWIS_LATEST
-__all__ = ["SimulatedIps"]
--- lewis_emulators/ips/interfaces/__init__.py
+++
@@ -1,5 +0,0 @@
-# from .stream_interface import IpsStreamInterface
-from .stream_interface_scpi import IpsStreamInterface
-
-# __all__ = ["IpsStreamInterface", "IpsSCPIStreamInterface"]
-__all__ = ["IpsStreamInterface"]
